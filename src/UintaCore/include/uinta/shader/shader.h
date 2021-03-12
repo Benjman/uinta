43fbd4f2a6dcb416901390ac24684656623543f4
Ben <benjamin.s.record@gmail.com>
2021-03-11 22:25:35 -0700

    using shaders

--- src/UintaCore/include/uinta/shader/shader.h
+++ src/UintaCore/include/uinta/shader/shader.h
@@ -14,12 +14,16 @@ namespace uinta {
 	public:
 		static Shader createShader(ShaderDto &dto);
 
-		[[nodiscard]] program_id_t getProgramId() const { return _programId; }
+		[[nodiscard]] program_id_t getProgramId() const { return _id; }
+
+		void use() const;
+
+		~Shader();
 
 	private:
-		explicit Shader(program_id_t programId) : _programId(programId) {}
+		explicit Shader(program_id_t programId) : _id(programId) {}
 
-		program_id_t _programId;
+		program_id_t _id;
 
 	};
 
--- src/UintaCore/src/shader/shader.cpp
+++ src/UintaCore/src/shader/shader.cpp
@@ -2,7 +2,18 @@
 
 #include <uinta/shader/shader.h>
 #include <uinta/shader/shader_dto.h>
+#include <uinta/gl/gl_error.h>
 
 uinta::Shader uinta::Shader::createShader(ShaderDto &dto) {
 	return ShaderLoader::loadShader(dto);
+}
+
+void uinta::Shader::use() const {
+	// TODO check GL state
+	glUseProgram(_id);
+	glCheckError(GL_USE_PROGRAM);
+}
+
+uinta::Shader::~Shader() {
+	glDeleteShader(_id);
 }
