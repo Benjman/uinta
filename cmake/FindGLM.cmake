6ad41fdeb51307fd15e51a4d9e3241d2695fabb5
Ben <benjamin.s.record@gmail.com>
2021-03-13 09:57:35 -0700

    hello stb

--- cmake/FindGLM.cmake
+++ cmake/FindGLM.cmake
@@ -1,10 +1,10 @@
-# This script should work as long as GLM was built by running /path/to/engine/configure
+# This script should work as long as STB was built by running /path/to/engine/configure
 
-find_path(GLM_INCLUDE names "glm/glm.hpp" PATHS "${CMAKE_SOURCE_DIR}/dep/glm")
+find_path(STB_INCLUDE names "stb.h" PATHS "${CMAKE_SOURCE_DIR}/dep/stb")
 
-if (NOT GLM_INCLUDE)
-    message(FATAL_ERROR "Failed to locate GLM. Possibly haven't run the configuration script?")
+if (NOT STB_INCLUDE)
+    message(FATAL_ERROR "Failed to locate STB. Possibly haven't run the configuration script?")
 endif()
 
 include(FindPackageHandleStandardArgs)
-find_package_handle_standard_args(GLM DEFAULT_MSG GLM_INCLUDE)
+find_package_handle_standard_args(STB DEFAULT_MSG STB_INCLUDE)
