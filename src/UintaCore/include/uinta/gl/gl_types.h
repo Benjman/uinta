561c8bcb7da561778e0a327ba1c21d4af07fcd1b
Ben <benjamin.s.record@gmail.com>
2021-03-14 14:04:08 -0600

    more types

--- src/UintaCore/include/uinta/gl/gl_types.h
+++ src/UintaCore/include/uinta/gl/gl_types.h
@@ -5,28 +5,19 @@
 
 namespace uinta {
 
-	using gl_type_t = GLenum;
-
-	using location_id_t = GLint;
-	using program_id_t = GLuint;
-
-	// VBO
-	using vbo_id_t = GLuint;
-	using vbo_target_t = GLenum;
-	using vbo_usage_t = GLenum;
-	using vbo_size_t = GLsizeiptr;
-
-	// VAO
-	using vao_id_t = GLuint;
-
-	// Vertex Attribute
 	using attrib_index_t = GLuint;
 	using attrib_normalize_t = GLboolean;
-	using attrib_stride_t = GLsizei;
 	using attrib_size_t = GLint;
-
-
-
+	using attrib_stride_t = GLsizei;
+	using gl_type_t = GLenum;
+	using location_id_t = GLint;
+	using program_id_t = GLuint;
+	using texture_id_t = GLuint;
+	using vao_id_t = GLuint;
+	using vbo_id_t = GLuint;
+	using vbo_size_t = GLsizeiptr;
+	using vbo_target_t = GLenum;
+	using vbo_usage_t = GLenum;
 
 	static inline const location_id_t INVALID_VBO_ID = 0;
 
