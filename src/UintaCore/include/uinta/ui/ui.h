a99d49b5ddd4b38fef8a5019ef04749bae3927cc
Ben <benjamin.s.record@gmail.com>
2021-03-22 18:33:48 -0600

    added main ui element

--- src/UintaCore/include/uinta/ui/ui.h
+++ src/UintaCore/include/uinta/ui/ui.h
@@ -3,4 +3,11 @@
 
 #define UI_BASE_SIZE 1080.f
 
+namespace uinta {
+	class UiElement;
+	extern UiElement *MAIN_UI_ELEMENT;
+
+	// TODO create main font global variable, and reference it in the Text class
+}
+
 #endif //UINTA_UI_H
---
+++ src/UintaCore/src/ui/ui.cpp
@@ -0,0 +1,5 @@
+#include <uinta/ui.h>
+
+namespace uinta {
+	UiElement *MAIN_UI_ELEMENT = new UiElement(nullptr, 0, 0, UI_BASE_SIZE, SIZE_MAX, 0); // NOLINT(cert-err58-cpp)
+}
