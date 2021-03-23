67f6c6c7a4113b0de604ebc70617e07408089e82
Ben <benjamin.s.record@gmail.com>
2021-03-22 18:32:25 -0600

    utility macros for converting from unit domain to ndc

--- src/UintaCore/include/uinta/gl.h
+++ src/UintaCore/include/uinta/gl.h
@@ -1,3 +1,11 @@
 #include "gl/gl_error.h"
 #include "gl/gl_state.h"
 #include "gl/gl_types.h"
+
+#ifndef TO_GL_NDC_X
+#define TO_GL_NDC_X(x) (2 * x - 1)
+#endif // TO_GL_NDC_X
+
+#ifndef TO_GL_NDC_Y
+#define TO_GL_NDC_Y(y) (2 * y - 1)
+#endif // TO_GL_NDC_Y
