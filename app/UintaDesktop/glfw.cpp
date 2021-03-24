b0b7b5a068881a412b50936579e0f524131cd7d6
Ben <benjamin.s.record@gmail.com>
2021-03-23 19:49:52 -0600

    formatting

--- app/UintaDesktop/glfw.cpp
+++ app/UintaDesktop/glfw.cpp
@@ -7,128 +7,131 @@
 #include <map>
 #include <utility>
 
-using namespace uinta::glfw;
+namespace uinta::glfw {
 
-namespace uinta::internal {
-	using WindowMap = std::map<GLFWwindow *, GlfwDto *>;
-	extern WindowMap windows;
-}
+	using namespace uinta::glfw;
 
-uinta::internal::WindowMap uinta::internal::windows;
-
-bool uinta::glfw::initialize(GlfwDto &dto) {
-	initializeGlfw(dto);
-	if (dto.getStatus() != Initialized) {
-		// TODO fatal logging
-		std::cerr << "Failed to initialize GLFW." << std::endl;
-		return false;
+	namespace internal {
+		using WindowMap = std::map<GLFWwindow *, GlfwDto *>;
+		extern WindowMap windows;
 	}
 
-	createWindow(dto);
-	if (dto.getStatus() != WindowCreated) {
-		// TODO fatal logging
-		std::cerr << "Failed to create GLFW _window." << std::endl;
-		return false;
+	internal::WindowMap internal::windows;
+
+	bool initialize(GlfwDto &dto) {
+		initializeGlfw(dto);
+		if (dto.getStatus() != Initialized) {
+			// TODO fatal logging
+			std::cerr << "Failed to initialize GLFW." << std::endl;
+			return false;
+		}
+
+		createWindow(dto);
+		if (dto.getStatus() != WindowCreated) {
+			// TODO fatal logging
+			std::cerr << "Failed to create GLFW _window." << std::endl;
+			return false;
+		}
+
+		initializeGlad(dto);
+		if (dto.getStatus() != GladInitialized) {
+			// TODO fatal logging
+			std::cerr << "Failed to initialize GLAD." << std::endl;
+			return false;
+		}
+
+		gl_state::setViewportSize(dto.getWidth(), dto.getHeight());
+
+		std::cout << "Initialization complete" << std::endl;
+		printInfo();
+
+		return true;
 	}
 
-	initializeGlad(dto);
-	if (dto.getStatus() != GladInitialized) {
-		// TODO fatal logging
-		std::cerr << "Failed to initialize GLAD." << std::endl;
-		return false;
+	void createWindow(GlfwDto &dto) {
+		std::cout << "Creating " << (dto.isHeadless() ? "headless " : "") << "window... ";
+
+		glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+		glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+		glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+		glfwWindowHint(GLFW_VISIBLE, !dto.isHeadless());
+
+		GLFWwindow *window = glfwCreateWindow(dto.getWidth(),
+											  dto.getHeight(),
+											  dto.getTitle().c_str(),
+											  nullptr, nullptr);
+
+		if (window == nullptr) {
+			dto.setStatus(Error);
+			return;
+		}
+
+		dto.setWindow(window);
+		dto.setStatus(WindowCreated);
+		internal::windows.insert(std::pair<GLFWwindow *, GlfwDto *>(window, &dto));
+
+		glfwSetFramebufferSizeCallback(dto.getWindow(), framebufferSizeChangedHandler);
+		setContext(dto);
+
+		std::cout << "done" << std::endl;
 	}
 
-	gl_state::setViewportSize(dto.getWidth(), dto.getHeight());
+	void framebufferSizeChangedHandler(GLFWwindow *window, int width, int height) {
+		GlfwDto *&dto = internal::windows[window];
+		if (dto == nullptr) {
+			// TODO warn logging
+			std::cerr << "Failed to respond to window resizing\n";
+		}
 
-	std::cout << "Initialization complete" << std::endl;
-	printInfo();
-
-	return true;
-}
-
-void uinta::glfw::createWindow(GlfwDto &dto) {
-	std::cout << "Creating " << (dto.isHeadless() ? "headless " : "") << "window... ";
-
-	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
-	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
-	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
-	glfwWindowHint(GLFW_VISIBLE, !dto.isHeadless());
-
-	GLFWwindow *window = glfwCreateWindow(dto.getWidth(),
-										  dto.getHeight(),
-										  dto.getTitle().c_str(),
-										  nullptr, nullptr);
-
-	if (window == nullptr) {
-		dto.setStatus(Error);
-		return;
+		dto->setViewportSize(width, height);
+		setGlViewport(*dto);
+		gl_state::setViewportSize(width, height);
 	}
 
-	dto.setWindow(window);
-	dto.setStatus(WindowCreated);
-	internal::windows.insert(std::pair<GLFWwindow *, GlfwDto *>(window, &dto));
-
-	glfwSetFramebufferSizeCallback(dto.getWindow(), framebufferSizeChangedHandler);
-	setContext(dto);
-
-	std::cout << "done" << std::endl;
-}
-
-void uinta::glfw::framebufferSizeChangedHandler(GLFWwindow *window, int width, int height) {
-	GlfwDto *&dto = internal::windows[window];
-	if (dto == nullptr) {
-		// TODO warn logging
-		std::cerr << "Failed to respond to window resizing\n";
+	void glfwErrorHandler(int error, const char *description) {
+		std::cerr << "GLFW Error:\n\t" << description << "\n\n";
 	}
 
-	dto->setViewportSize(width, height);
-	setGlViewport(*dto);
-	gl_state::setViewportSize(width, height);
-}
-
-void uinta::glfw::glfwErrorHandler(int error, const char *description) {
-	std::cerr << "GLFW Error:\n\t" << description << "\n\n";
-}
-
-void uinta::glfw::initializeGlfw(GlfwDto &dto) {
-	std::cout << "Initializing GLFW... ";
-	if (!glfwInit()) {
-		dto.setStatus(Error);
+	void initializeGlfw(GlfwDto &dto) {
+		std::cout << "Initializing GLFW... ";
+		if (!glfwInit()) {
+			dto.setStatus(Error);
+		}
+		glfwSetErrorCallback(&glfwErrorHandler);
+		dto.setStatus(Initialized);
+		std::cout << "done" << std::endl;
 	}
-	glfwSetErrorCallback(&glfwErrorHandler);
-	dto.setStatus(Initialized);
-	std::cout << "done" << std::endl;
-}
 
-void uinta::glfw::initializeGlad(GlfwDto &dto) {
-	std::cout << "Initializing GLAD... ";
-	if (!gladLoadGLES2Loader((GLADloadproc) glfwGetProcAddress)) {
-		dto.setStatus(Error);
+	void initializeGlad(GlfwDto &dto) {
+		std::cout << "Initializing GLAD... ";
+		if (!gladLoadGLES2Loader((GLADloadproc) glfwGetProcAddress)) {
+			dto.setStatus(Error);
+		}
+		dto.setStatus(GladInitialized);
+		std::cout << "done" << std::endl;
 	}
-	dto.setStatus(GladInitialized);
-	std::cout << "done" << std::endl;
-}
 
-void uinta::glfw::printInfo() {
-	std::cout << "BUILD TYPE: " << (uintaIsDebugBuild() ? "Debug" : "Release") << std::endl;
-	std::cout << "GLFW VERSION: " << glfwGetVersionString() << std::endl;
-	std::cout << "OPENGL VERSION: " << glGetString(GL_VERSION) << std::endl;
-	std::cout << "TARGET: " << uintaGetOsName() << std::endl;
-	std::cout << "THREADS: " << uintaGetHardwareConcurrencyCount() << std::endl;
-}
+	void printInfo() {
+		std::cout << "BUILD TYPE: " << (uintaIsDebugBuild() ? "Debug" : "Release") << std::endl;
+		std::cout << "GLFW VERSION: " << glfwGetVersionString() << std::endl;
+		std::cout << "OPENGL VERSION: " << glGetString(GL_VERSION) << std::endl;
+		std::cout << "TARGET: " << uintaGetOsName() << std::endl;
+		std::cout << "THREADS: " << uintaGetHardwareConcurrencyCount() << std::endl;
+	}
 
-void uinta::glfw::setContext(GlfwDto &dto) {
-	glfwMakeContextCurrent(dto.getWindow());
-}
+	void setContext(GlfwDto &dto) {
+		glfwMakeContextCurrent(dto.getWindow());
+	}
 
-void uinta::glfw::setGlViewport(GlfwDto &dto) {
-	glViewport(0, 0, dto.getWidth(), dto.getHeight());
-}
+	void setGlViewport(GlfwDto &dto) {
+		glViewport(0, 0, dto.getWidth(), dto.getHeight());
+	}
 
-bool uinta::glfw::shouldClose(GlfwDto &dto) {
-	return glfwWindowShouldClose(dto.getWindow());
-}
+	bool shouldClose(GlfwDto &dto) {
+		return glfwWindowShouldClose(dto.getWindow());
+	}
 
-void uinta::glfw::terminate() {
-	glfwTerminate();
+	void terminate() {
+		glfwTerminate();
+	}
 }
--- app/UintaDesktop/glfw.h
+++ app/UintaDesktop/glfw.h
@@ -8,105 +8,105 @@
 
 namespace uinta::glfw {
 
-    enum GlfwStatus {
-        Error,
-        Initializing,
-        Initialized,
-        WindowCreated,
-        GladInitialized
-    };
+	enum GlfwStatus {
+		Error,
+		Initializing,
+		Initialized,
+		WindowCreated,
+		GladInitialized
+	};
 
-    class GlfwDto {
-        GlfwStatus _status = Initializing;
-        GLFWwindow *_window = nullptr;
-        int32_t _width = 0,
-                _height = 0;
-        std::string _title;
-        bool _headless = false;
-        bool _viewportChanged = false;
+	class GlfwDto {
+		GlfwStatus _status = Initializing;
+		GLFWwindow *_window = nullptr;
+		int32_t _width = 0,
+				_height = 0;
+		std::string _title;
+		bool _headless = false;
+		bool _viewportChanged = false;
 
-    public:
-        GlfwDto(int32_t width, int32_t height, std::string title)
-                : _width(width), _height(height), _title(std::move(title)) {
-        }
+	public:
+		GlfwDto(int32_t width, int32_t height, std::string title)
+				: _width(width), _height(height), _title(std::move(title)) {
+		}
 
-        [[nodiscard]] GlfwStatus getStatus() const {
-            return _status;
-        }
+		[[nodiscard]] GlfwStatus getStatus() const {
+			return _status;
+		}
 
-        void setStatus(GlfwStatus status) {
-            _status = status;
-        }
+		void setStatus(GlfwStatus status) {
+			_status = status;
+		}
 
-        [[nodiscard]] GLFWwindow *getWindow() const {
-            return _window;
-        }
+		[[nodiscard]] GLFWwindow *getWindow() const {
+			return _window;
+		}
 
-        void setWindow(GLFWwindow *window) {
-            _window = window;
-        }
+		void setWindow(GLFWwindow *window) {
+			_window = window;
+		}
 
-        int32_t &getWidth() {
-            return _width;
-        }
+		int32_t &getWidth() {
+			return _width;
+		}
 
-        int32_t &getHeight() {
-            return _height;
-        }
+		int32_t &getHeight() {
+			return _height;
+		}
 
-        void setViewportSize(int32_t width, int32_t height) {
-        	_viewportChanged = _width != width || _height != height;
-            _width = width;
-            _height = height;
-        }
+		void setViewportSize(int32_t width, int32_t height) {
+			_viewportChanged = _width != width || _height != height;
+			_width = width;
+			_height = height;
+		}
 
-        std::string &getTitle() {
-            return _title;
-        }
+		std::string &getTitle() {
+			return _title;
+		}
 
-        void setTitle(const std::string &title) {
-            _title = title;
-        }
+		void setTitle(const std::string &title) {
+			_title = title;
+		}
 
-        [[nodiscard]] bool isHeadless() const {
-            return _headless;
-        }
+		[[nodiscard]] bool isHeadless() const {
+			return _headless;
+		}
 
-        void setHeadless(bool headless) {
-            _headless = headless;
-        }
+		void setHeadless(bool headless) {
+			_headless = headless;
+		}
 
 		[[nodiscard]] bool isViewportChanged() const {
 			return _viewportChanged;
 		}
 
 		void resetViewportChanged() {
-        	_viewportChanged = false;
-        }
+			_viewportChanged = false;
+		}
 
 	};
 
-    extern bool initialize(GlfwDto &dto);
+	extern bool initialize(GlfwDto &dto);
 
-    extern void createWindow(GlfwDto &dto);
+	extern void createWindow(GlfwDto &dto);
 
-    extern void framebufferSizeChangedHandler(GLFWwindow *window, int width, int height);
+	extern void framebufferSizeChangedHandler(GLFWwindow *window, int width, int height);
 
-    extern void glfwErrorHandler(int error, const char *description);
+	extern void glfwErrorHandler(int error, const char *description);
 
-    extern void initializeGlad(GlfwDto &dto);
+	extern void initializeGlad(GlfwDto &dto);
 
-    extern void initializeGlfw(GlfwDto &dto);
+	extern void initializeGlfw(GlfwDto &dto);
 
-    extern void printInfo();
+	extern void printInfo();
 
-    extern void setContext(GlfwDto &dto);
+	extern void setContext(GlfwDto &dto);
 
-    extern void setGlViewport(GlfwDto &dto);
+	extern void setGlViewport(GlfwDto &dto);
 
-    extern bool shouldClose(GlfwDto &dto);
+	extern bool shouldClose(GlfwDto &dto);
 
-    extern void terminate();
+	extern void terminate();
 
 }
 
