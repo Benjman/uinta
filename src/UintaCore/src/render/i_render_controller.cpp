faf16d334138f630d8e945d0ba36715c47407984
Ben <benjamin.s.record@gmail.com>
2021-04-09 20:49:35 -0600

    early exit for no renderables case

--- src/UintaCore/src/render/i_render_controller.cpp
+++ src/UintaCore/src/render/i_render_controller.cpp
@@ -3,6 +3,7 @@
 using namespace uinta;
 
 void IRenderController::render() {
+	if (_numRenderables == 0) return;
 	for (auto renderable : _renderables) {
 		if (renderable != nullptr) {
 			// TODO batch rendering?
