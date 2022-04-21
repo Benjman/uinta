4caa1d02a2ef45484425e380bdd7a6e57f2b2897
Ben <benjamin.s.record@gmail.com>
2022-04-20 20:37:36 -0600

    render timing running average

--- app/showcase/main.cpp
+++ app/showcase/main.cpp
@@ -4,6 +4,8 @@
 #include "./runner.hpp"
 #include "../utils/debug_controller.hpp"
 
+#include <math.hpp>
+
 showcaseRunner runner;
 
 void key_callback(GLFWwindow* window, int key, int scancode, int action, int mods) noexcept;
@@ -11,15 +13,17 @@ void key_callback(GLFWwindow* window, int key, int scancode, int action, int mod
 int main(const int argc, const char** argv) {
     debug_controller debug(WINDOW_WIDTH, WINDOW_HEIGHT);
 
-    metric_t m_render   = debug.metrics.init_metric(METRIC_FLOAT, "render");
-    metric_t m_tick     = debug.metrics.init_metric(METRIC_UINT, "tick");
+    metric_t m_render       = debug.metrics.init_metric(METRIC_FLOAT, "render avg (10)");
+    metric_t m_tick         = debug.metrics.init_metric(METRIC_UINT, "tick");
 
     debug_timer_t t_render = debug.create_timer("render");
 
-    runner.init();
-    debug.init();
+    running_avg render_avg(10);
 
+    runner.init();
     glfwSetKeyCallback(runner.view.window, key_callback);
+
+    debug.init();
 
     double dt = 0.0;
     double time = 0.0;
@@ -35,12 +39,15 @@ int main(const int argc, const char** argv) {
         glClear(GL_COLOR_BUFFER_BIT);
 
         debug.mesh_metric(m_tick);
-        debug.mesh_metric(m_render, "us");
+        debug.mesh_metric(m_render);
 
         debug.reset_timer(t_render);
         runner.render();
         debug.render();
         debug.metrics.set(m_render, (float) debug.duration_micro(t_render));
+
+        render_avg.add((float) debug.duration_micro(t_render));
+        debug.metrics.set(m_render, render_avg.avg());
 
         glfwSwapBuffers(runner.view.window);
         glfwPollEvents();
