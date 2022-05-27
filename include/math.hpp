3f6f3c6d87de22d919a23da71d1b73f529f4c6dd
Ben <benjamin.s.record@gmail.com>
2022-05-27 12:08:39 -0600

    removed custom geometry in favor of glm

--- include/math.hpp
+++ include/math.hpp
@@ -1,150 +1,6 @@
 #ifndef UINTA_MATH_H
 #define UINTA_MATH_H
 
-struct vec2 final {
-    float x;
-    float y;
-
-    vec2() noexcept;
-    vec2(float v) noexcept;
-    vec2(float x, float y) noexcept;
-    vec2(const vec2 &vec) noexcept;
-
-    vec2& operator=(const vec2 &rhs) noexcept;
-    vec2 operator*(const vec2 &rhs) const noexcept;
-    vec2 operator*(float v) const noexcept;
-    vec2 operator*=(const vec2 &rhs) noexcept;
-    vec2 operator+(const vec2 &rhs) const noexcept;
-    vec2 operator-(const vec2 &rhs) const noexcept;
-    vec2 operator/(const vec2 &rhs) noexcept;
-    vec2 operator/=(const vec2 &rhs) noexcept;
-
-    bool operator!=(const vec2 &rhs) const noexcept;
-    bool operator<(const vec2 &rhs) const noexcept;
-    bool operator<=(const vec2 &rhs) const noexcept;
-    bool operator==(const vec2 &rhs) const noexcept;
-    bool operator>(const vec2 &rhs) const noexcept;
-    bool operator>=(const vec2 &rhs) const noexcept;
-
-};
-
-struct vec3 final {
-    float x;
-    float y;
-    float z;
-
-    vec3() noexcept;
-    vec3(float v) noexcept;
-    vec3(float x, float y, float z) noexcept;
-    vec3(const vec3& vec) noexcept;
-    vec3(const vec2& vec) noexcept;
-    vec3(const vec2& vec, float z) noexcept;
-
-    vec3& operator=(const vec3& vec) noexcept;
-    vec3 operator*(const vec3 &rhs) const noexcept;
-    vec3 operator*(float v) const noexcept;
-    vec3 operator*=(const vec3 &rhs) noexcept;
-    vec3 operator+(const vec3 &other) noexcept;
-    vec3 operator-(const vec3 &other) noexcept;
-    vec3 operator/(const vec3 &rhs) noexcept;
-    vec3 operator/=(const vec3 &rhs) noexcept;
-
-    bool operator!=(const vec3 &rhs) const noexcept;
-    bool operator<(const vec3 &rhs) const noexcept;
-    bool operator<=(const vec3 &rhs) const noexcept;
-    bool operator==(const vec3 &rhs) const noexcept;
-    bool operator>(const vec3 &rhs) const noexcept;
-    bool operator>=(const vec3 &rhs) const noexcept;
-
-};
-
-struct vec4 final {
-    float x;
-    float y;
-    float z;
-    float w;
-
-    vec4() noexcept : vec4(0.0) {}
-    vec4(float v) noexcept : vec4(v, v, v, v) {}
-    vec4(float x, float y, float z, float w) noexcept;
-    vec4(const vec4& vec) noexcept;
-    vec4(const vec2& vec) noexcept;
-    vec4(const vec2& vec, float z) noexcept;
-    vec4(const vec2& vec, float z, float w) noexcept;
-    vec4(const vec3& vec) noexcept;
-    vec4(const vec3& vec, float w) noexcept;
-
-    vec4& operator=(const vec4& vec) noexcept;
-    vec4 operator*(const vec4 &rhs) const noexcept;
-    vec4 operator*(float v) const noexcept;
-    vec4 operator*=(const vec4 &rhs) noexcept;
-    vec4 operator+(const vec4 &other) noexcept;
-    vec4 operator-(const vec4 &other) noexcept;
-    vec4 operator/(const vec4 &rhs) noexcept;
-    vec4 operator/=(const vec4 &rhs) noexcept;
-
-    bool operator!=(const vec4 &rhs) const noexcept;
-    bool operator<(const vec4 &rhs) const noexcept;
-    bool operator<=(const vec4 &rhs) const noexcept;
-    bool operator==(const vec4 &rhs) const noexcept;
-    bool operator>(const vec4 &rhs) const noexcept;
-    bool operator>=(const vec4 &rhs) const noexcept;
-
-};
-
-/** matricies are column major **/
-struct mat4 final {
-    float values[16];
-
-    mat4();
-
-    mat4(float v) noexcept;
-    mat4(const float *values) noexcept;
-    mat4(const mat4 &other) noexcept;
-    mat4(const vec4 &other) noexcept;
-
-    mat4& operator=(const mat4 &rhs) noexcept;
-
-    mat4& operator*(const vec4 &rhs) noexcept;
-    mat4& operator*=(const vec4 &rhs) noexcept;
-    mat4& operator=(const vec4 &rhs) noexcept;
-
-    float m00() { return values[0]; }
-    void m00(float v) { values[0] = v; }
-    float m01() { return values[1]; }
-    void m01(float v) { values[1] = v; }
-    float m02() { return values[2]; }
-    void m02(float v) { values[2] = v; }
-    float m03() { return values[3]; }
-    void m03(float v) { values[3] = v; }
-    float m10() { return values[4]; }
-    void m10(float v) { values[4] = v; }
-    float m11() { return values[5]; }
-    void m11(float v) { values[5] = v; }
-    float m12() { return values[6]; }
-    void m12(float v) { values[6] = v; }
-    float m13() { return values[7]; }
-    void m13(float v) { values[7] = v; }
-    float m20() { return values[8]; }
-    void m20(float v) { values[8] = v; }
-    float m21() { return values[9]; }
-    void m21(float v) { values[9] = v; }
-    float m22() { return values[10]; }
-    void m22(float v) { values[10] = v; }
-    float m23() { return values[11]; }
-    void m23(float v) { values[11] = v; }
-    float m30() { return values[12]; }
-    void m30(float v) { values[12] = v; }
-    float m31() { return values[13]; }
-    void m31(float v) { values[13] = v; }
-    float m32() { return values[14]; }
-    void m32(float v) { values[14] = v; }
-    float m33() { return values[15]; }
-    void m33(float v) { values[15] = v; }
-
-};
-
-#include <algorithm>
 struct running_avg final {
 public:
     unsigned int count;
@@ -174,22 +30,55 @@ struct smooth_float final {
     float current;
     float target;
 
-    smooth_float(const float agility = 1.0, const float target = 0.0) noexcept;
+    smooth_float(const float start, const float agility) noexcept;
     smooth_float(const smooth_float& other) noexcept;
 
     smooth_float& operator=(const smooth_float& other) noexcept;
-
+    smooth_float& operator=(const float v) noexcept;
     smooth_float& operator+=(const float v) noexcept;
-
     smooth_float& operator-=(const float v) noexcept;
-
-    operator float() const noexcept;
 
     void force() noexcept;
     void force(float v) noexcept;
 
     void tick(const float dt) noexcept;
 
+};
+
+#include <glm/vec3.hpp>
+struct smooth_vec3 {
+    smooth_float values[3] {
+        smooth_float(0.0, 5.0),
+        smooth_float(0.0, 5.0),
+        smooth_float(0.0, 5.0)
+    };
+
+    smooth_vec3(const smooth_float& x = smooth_float(0.0, 5.0), const smooth_float& y = smooth_float(0.0, 5.0), const smooth_float& z = smooth_float(0.0, 5.0)) {
+        values[0] = x;
+        values[1] = y;
+        values[2] = z;
+    }
+
+    float x() { return smooth_float_x().current; }
+    void x(float v) { smooth_float_x().target = v; }
+
+    float y() { return smooth_float_y().current; }
+    void y(float v) { smooth_float_y().target = v; }
+
+    float z() { return smooth_float_z().current; }
+    void z(float v) { smooth_float_z().target = v; }
+
+    smooth_float& smooth_float_x() { return values[0]; }
+    smooth_float& smooth_float_y() { return values[1]; }
+    smooth_float& smooth_float_z() { return values[2]; }
+
+    void tick(const float dt) {
+        smooth_float_x().tick(dt);
+        smooth_float_y().tick(dt);
+        smooth_float_z().tick(dt);
+    }
+
+    // TODO math operators against glm::vec3
 };
 
 #endif // UINTA_MATH_H
--- src/math.cpp
+++ src/math.cpp
@@ -1,355 +1,5 @@
 #include <math.hpp>
 
-vec2::vec2() noexcept : vec2(0.0) {}
-
-vec2::vec2(float v) noexcept : vec2(v, v) {}
-
-vec2::vec2(float x, float y) noexcept {
-    this->x = x;
-    this->y = y;
-}
-
-vec2::vec2(const vec2 &vec) noexcept {
-    *this = vec;
-}
-
-vec2& vec2::operator=(const vec2 &rhs) noexcept {
-    x = rhs.x;
-    y = rhs.y;
-    return *this;
-}
-
-vec2 vec2::operator+(const vec2 &rhs) const noexcept {
-    return vec2(x + rhs.x, y + rhs.y);
-}
-
-vec2 vec2::operator-(const vec2 &rhs) const noexcept {
-    return vec2(x - rhs.x, y - rhs.y);
-}
-
-vec2 vec2::operator*(const vec2 &rhs) const noexcept {
-    return vec2(x * rhs.x, y * rhs.y);
-}
-
-vec2 vec2::operator*(float v) const noexcept {
-    return vec2(x * v, y * v);
-}
-
-vec2 vec2::operator*=(const vec2 &rhs) noexcept {
-    x *= rhs.x;
-    y *= rhs.y;
-    return *this;
-}
-
-vec2 vec2::operator/(const vec2 &rhs) noexcept {
-    return vec2(x / rhs.x, y / rhs.y);
-}
-
-vec2 vec2::operator/=(const vec2 &rhs) noexcept {
-    x /= rhs.x;
-    y /= rhs.y;
-    return *this;
-}
-
-bool vec2::operator>(const vec2 &rhs) const noexcept {
-    return x > rhs.x && y > rhs.y;
-}
-
-bool vec2::operator<(const vec2 &rhs) const noexcept {
-    return x < rhs.x && y < rhs.y;
-}
-
-bool vec2::operator>=(const vec2 &rhs) const noexcept {
-    return x >= rhs.x && y >= rhs.y;
-}
-
-bool vec2::operator<=(const vec2 &rhs) const noexcept {
-    return x <= rhs.x && y <= rhs.y;
-}
-
-bool vec2::operator==(const vec2 &rhs) const noexcept {
-    return x == rhs.x && y == rhs.y;
-}
-
-bool vec2::operator!=(const vec2 &rhs) const noexcept {
-    return x != rhs.x || y != rhs.y;
-}
-
-vec3::vec3() noexcept : vec3(0.0) {}
-
-vec3::vec3(float v) noexcept : vec3(v, v, v) {}
-
-vec3::vec3(float x, float y, float z) noexcept {
-    this->x = x;
-    this->y = y;
-    this->z = z;
-}
-
-vec3::vec3(const vec3& vec) noexcept {
-    *this = vec;
-}
-
-vec3::vec3(const vec2& vec) noexcept : vec3(vec, 0.0) {}
-
-vec3::vec3(const vec2& vec, float z) noexcept : vec3(vec.x, vec.y, z) {}
-
-vec3& vec3::operator=(const vec3& vec) noexcept {
-    x = vec.x;
-    y = vec.y;
-    z = vec.z;
-    return *this;
-}
-
-vec3 vec3::operator+(const vec3 &other) noexcept {
-    return vec3(x + other.x, y + other.y, z + other.z);
-}
-
-vec3 vec3::operator-(const vec3 &other) noexcept {
-    return vec3(x - other.x, y - other.y, z - other.z);
-}
-
-vec3 vec3::operator*(const vec3 &rhs) const noexcept {
-    return vec3(x * rhs.x, y * rhs.y, z * rhs.z);
-}
-
-vec3 vec3::operator*(float v) const noexcept {
-    return vec3(x * v, y * v, z * v);
-}
-
-vec3 vec3::operator*=(const vec3 &rhs) noexcept {
-    x *= rhs.x;
-    y *= rhs.y;
-    z *= rhs.z;
-    return *this;
-}
-
-vec3 vec3::operator/(const vec3 &rhs) noexcept {
-    return vec3(x / rhs.x, y / rhs.y, z / rhs.z);
-}
-
-vec3 vec3::operator/=(const vec3 &rhs) noexcept {
-    x /= rhs.x;
-    y /= rhs.y;
-    z /= rhs.z;
-    return *this;
-}
-
-bool vec3::operator>(const vec3 &rhs) const noexcept {
-    return x > rhs.x && y > rhs.y && z > rhs.z;
-}
-
-bool vec3::operator<(const vec3 &rhs) const noexcept {
-    return x < rhs.x && y < rhs.y && z < rhs.z;
-}
-
-bool vec3::operator>=(const vec3 &rhs) const noexcept {
-    return x >= rhs.x && y >= rhs.y && z >= rhs.z;
-}
-
-bool vec3::operator<=(const vec3 &rhs) const noexcept {
-    return x <= rhs.x && y <= rhs.y && z <= rhs.z;
-}
-
-bool vec3::operator==(const vec3 &rhs) const noexcept {
-    return x == rhs.x && y == rhs.y && z == rhs.z;
-}
-
-bool vec3::operator!=(const vec3 &rhs) const noexcept {
-    return x != rhs.x || y != rhs.y || z == rhs.z;
-}
-
-vec4::vec4(float x, float y, float z, float w) noexcept {
-    this->x = x;
-    this->y = y;
-    this->z = z;
-    this->w = w;
-}
-
-vec4::vec4(const vec4& vec) noexcept {
-    *this = vec;
-}
-
-vec4::vec4(const vec2& vec) noexcept : vec4(vec, 0.0, 0.0) {}
-
-vec4::vec4(const vec2& vec, float z) noexcept : vec4(vec.x, vec.y, z, 0.0) {}
-
-vec4::vec4(const vec2& vec, float z, float w) noexcept : vec4(vec.x, vec.y, z, w) {}
-
-vec4::vec4(const vec3& vec) noexcept : vec4(vec, 0.0) {}
-
-vec4::vec4(const vec3& vec, float w) noexcept : vec4(vec.x, vec.y, vec.z, w) {}
-
-vec4& vec4::operator=(const vec4& vec) noexcept {
-    x = vec.x;
-    y = vec.y;
-    z = vec.z;
-    w = vec.w;
-    return *this;
-}
-
-vec4 vec4::operator+(const vec4 &other) noexcept {
-    return vec4(x + other.x, y + other.y, z + other.z, w + other.w);
-}
-
-vec4 vec4::operator-(const vec4 &other) noexcept {
-    return vec4(x - other.x, y - other.y, z - other.z, w - other.w);
-}
-
-vec4 vec4::operator*(const vec4 &rhs) const noexcept {
-    return vec4(x * rhs.x, y * rhs.y, z * rhs.z, w * rhs.w);
-}
-
-vec4 vec4::operator*(float v) const noexcept {
-    return vec4(x * v, y * v, z * v, w * v);
-}
-
-vec4 vec4::operator*=(const vec4 &rhs) noexcept {
-    x *= rhs.x;
-    y *= rhs.y;
-    z *= rhs.z;
-    w *= rhs.w;
-    return *this;
-}
-
-vec4 vec4::operator/(const vec4 &rhs) noexcept {
-    return vec4(x / rhs.x, y / rhs.y, z / rhs.z, w / rhs.w);
-}
-
-vec4 vec4::operator/=(const vec4 &rhs) noexcept {
-    x /= rhs.x;
-    y /= rhs.y;
-    z /= rhs.z;
-    w /= rhs.w;
-    return *this;
-}
-
-bool vec4::operator>(const vec4 &rhs) const noexcept {
-    return x > rhs.x && y > rhs.y && z > rhs.z && w > rhs.w;
-}
-
-bool vec4::operator<(const vec4 &rhs) const noexcept {
-    return x < rhs.x && y < rhs.y && z < rhs.z && w < rhs.w;
-}
-
-bool vec4::operator>=(const vec4 &rhs) const noexcept {
-    return x >= rhs.x && y >= rhs.y && z >= rhs.z && w >= rhs.w;
-}
-
-bool vec4::operator<=(const vec4 &rhs) const noexcept {
-    return x <= rhs.x && y <= rhs.y && z <= rhs.z && w <= rhs.w;
-}
-
-bool vec4::operator==(const vec4 &rhs) const noexcept {
-    return x == rhs.x && y == rhs.y && z == rhs.z && w == rhs.w;
-}
-
-bool vec4::operator!=(const vec4 &rhs) const noexcept {
-    return x != rhs.x || y != rhs.y || z == rhs.z && w != rhs.w;
-}
-
-#include <cstring> // for memcpy
-mat4::mat4(const float *values) noexcept {
-    /*
-       FIXME memcpy of matrices with different dimensions will dirty our array.
-             For example, a mat3 has 12 floats, but if it's sent to this constructor 
-             it will copy 16 floats, filling the tail of our array with garbage.
-    */
-    memcpy(this->values, values, sizeof(float) * 16);
-}
-
-mat4::mat4() {
-    values[0]  = 1.0;
-    values[1]  = 0.0;
-    values[2]  = 0.0;
-    values[3]  = 0.0;
-    values[4]  = 0.0;
-    values[5]  = 1.0;
-    values[6]  = 0.0;
-    values[7]  = 0.0;
-    values[8]  = 0.0;
-    values[9]  = 0.0;
-    values[10] = 1.0;
-    values[11] = 0.0;
-    values[12] = 0.0;
-    values[13] = 0.0;
-    values[14] = 0.0;
-    values[15] = 1.0;
-}
-
-mat4::mat4(float v) noexcept : mat4() {
-    values[0] = v;
-    values[5] = v;
-    values[10] = v;
-    values[15] = v;
-}
-
-mat4::mat4(const mat4 &other) noexcept {
-    *this = mat4(other.values);
-}
-
-mat4& mat4::operator=(const mat4 &rhs) noexcept {
-    memcpy(this->values, rhs.values, sizeof(float) * 16);
-    return *this;
-}
-
-mat4::mat4(const vec4 &other) noexcept : mat4() {
-    values[0] = other.x;
-    values[5] = other.y;
-    values[10] = other.z;
-    values[15] = other.w;
-}
-
-mat4& mat4::operator=(const vec4 &rhs) noexcept {
-    *this = mat4();
-    values[0] = rhs.x;
-    values[5] = rhs.y;
-    values[10] = rhs.z;
-    values[15] = rhs.w;
-    return *this;
-}
-
-mat4& mat4::operator*(const vec4 &rhs) noexcept {
-    float tmp[16];
-    memcpy(tmp, values, sizeof(tmp));
-    tmp[0]  *= rhs.x;
-    tmp[1]  *= rhs.x;
-    tmp[2]  *= rhs.x;
-    tmp[3]  *= rhs.x;
-    tmp[4]  *= rhs.y;
-    tmp[5]  *= rhs.y;
-    tmp[6]  *= rhs.y;
-    tmp[7]  *= rhs.y;
-    tmp[8]  *= rhs.z;
-    tmp[9]  *= rhs.z;
-    tmp[10] *= rhs.z;
-    tmp[11] *= rhs.z;
-    tmp[12] *= rhs.w;
-    tmp[13] *= rhs.w;
-    tmp[14] *= rhs.w;
-    tmp[15] *= rhs.w;
-    return *this;
-}
-
-mat4& mat4::operator*=(const vec4 &rhs) noexcept {
-    values[0] *= rhs.x;
-    values[1] *= rhs.x;
-    values[2] *= rhs.x;
-    values[3] *= rhs.x;
-    values[4] *= rhs.y;
-    values[5] *= rhs.y;
-    values[6] *= rhs.y;
-    values[7] *= rhs.y;
-    values[8] *= rhs.z;
-    values[9] *= rhs.z;
-    values[10] *= rhs.z;
-    values[11] *= rhs.z;
-    values[12] *= rhs.w;
-    values[13] *= rhs.w;
-    values[14] *= rhs.w;
-    values[15] *= rhs.w;
-    return *this;
-}
-
 running_avg::running_avg(const unsigned int sample_size) noexcept {
     buffer = new float[sample_size];
     mavg = 0.0;
@@ -379,6 +29,7 @@ running_avg::~running_avg() {
     delete[] buffer;
 }
 
+#include <algorithm> // std::min
 float running_avg::avg() noexcept {
     if (!cursor)
         return 0.0;
@@ -401,7 +52,7 @@ void running_avg::add(float v) noexcept {
     // TODO check for cursor violating uint max
 }
 
-smooth_float::smooth_float(const float agility, const float target) noexcept : agility(agility), current(target), target(target) {
+smooth_float::smooth_float(const float start, const float agility) noexcept : current(start), target(start), agility(agility) {
 }
 
 smooth_float::smooth_float(const smooth_float& other) noexcept {
@@ -415,6 +66,11 @@ smooth_float& smooth_float::operator=(const smooth_float& other) noexcept {
     return *this;
 }
 
+smooth_float& smooth_float::operator=(const float v) noexcept {
+    target = v;
+    return *this;
+}
+
 smooth_float& smooth_float::operator+=(const float v) noexcept {
     target += v;
     return *this;
@@ -423,10 +79,6 @@ smooth_float& smooth_float::operator+=(const float v) noexcept {
 smooth_float& smooth_float::operator-=(const float v) noexcept {
     target -= v;
     return *this;
-}
-
-smooth_float::operator float() const noexcept {
-    return current;
 }
 
 void smooth_float::force() noexcept {
@@ -438,6 +90,7 @@ void smooth_float::force(float v) noexcept {
     target = v;
 }
 
+#include <cstdio>
 void smooth_float::tick(const float dt) noexcept {
     float diff = (target - current);
     current += diff * agility * dt;
