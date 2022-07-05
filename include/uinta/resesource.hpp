42ce6b148a2de061a4586cb0d37c8476d48a5714
Ben <benjamin.s.record@gmail.com>
2022-07-05 16:51:53 -0600

    resource manager (not complete)

---
+++ include/uinta/resesource.hpp
@@ -0,0 +1,12 @@
+#ifndef UINTA_RESOURCE_HPP
+#define UINTA_RESOURCE_HPP
+
+#include <string>
+
+namespace uinta {
+namespace res {
+std::string *get_file(const std::string &);
+} // namespace res
+} // namespace uinta
+
+#endif // UINTA_RESOURCE_HPP
