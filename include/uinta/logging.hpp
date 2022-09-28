2fb91392e76cf7b0157b29bae32c2575d9bd751d
Ben <benjamin.s.record@gmail.com>
2022-09-28 13:55:32 -0600

    Logging level changes for quick select

--- include/uinta/logging.hpp
+++ include/uinta/logging.hpp
@@ -1,41 +1,17 @@
 #ifndef UINTA_LOGGING_HPP
 #define UINTA_LOGGING_HPP
 
-///
-/// spdlog levels for reference:
-///
-// #define SPDLOG_LEVEL_TRACE 0
-// #define SPDLOG_LEVEL_DEBUG 1
-// #define SPDLOG_LEVEL_INFO 2
-// #define SPDLOG_LEVEL_WARN 3
-// #define SPDLOG_LEVEL_ERROR 4
-// #define SPDLOG_LEVEL_CRITICAL 5
-// #define SPDLOG_LEVEL_OFF 6
-
 #ifdef SPDLOG_ACTIVE_LEVEL
 #undef SPDLOG_ACTIVE_LEVEL
 #endif  // SPDLOG_ACTIVE_LEVEL
 
-// // trace
-// #define SPDLOG_ACTIVE_LEVEL 0
-
-// debug
-#define SPDLOG_ACTIVE_LEVEL 1
-
-// // info
-// #define SPDLOG_ACTIVE_LEVEL 2
-
-// // warn
-// #define SPDLOG_ACTIVE_LEVEL 3
-
-// // error
-// #define SPDLOG_ACTIVE_LEVEL 4
-
-// // critical
-// #define SPDLOG_ACTIVE_LEVEL 5
-
-// // off
-// #define SPDLOG_ACTIVE_LEVEL 6
+// #define SPDLOG_ACTIVE_LEVEL 0 /* trace */
+// #define SPDLOG_ACTIVE_LEVEL 1 /* debug */
+#define SPDLOG_ACTIVE_LEVEL 2 /* info */
+// #define SPDLOG_ACTIVE_LEVEL 3 /* warn */
+// #define SPDLOG_ACTIVE_LEVEL 4 /* error */
+// #define SPDLOG_ACTIVE_LEVEL 5 /* critical */
+// #define SPDLOG_ACTIVE_LEVEL 6 /* off */
 
 #include <spdlog/spdlog.h>
 using logger_t = std::shared_ptr<spdlog::logger>;
