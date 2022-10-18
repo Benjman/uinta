ce0130c31c81fca18224f3beab7aaa18fb66476b
Ben <benjamin.s.record@gmail.com>
2022-10-18 14:45:05 -0600

    App source files moved to cmake to improve compile times

--- app/utils/utils.cpp
+++
@@ -1,9 +0,0 @@
-#ifndef UINTA_APP_UTILS_UTILS_CPP
-#define UINTA_APP_UTILS_UTILS_CPP
-
-// #include "./debug_controller.cpp"
-#include "./glfw_input.cpp"
-#include "./glfw_runner.cpp"
-#include "./imgui_util.cpp"
-
-#endif  // UINTA_APP_UTILS_UTILS_CPP
