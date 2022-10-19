08976562fc8161aa888dd8ba8fe8a83d195be0f8
Ben <benjamin.s.record@gmail.com>
2022-10-18 23:16:28 -0600

    Smoothing operators inlined

--- include/uinta/math/smooth_float.hpp
+++ include/uinta/math/smooth_float.hpp
@@ -8,21 +8,56 @@ struct SmoothFloat final {
   float current;
   float target;
 
-  SmoothFloat(float agility, float start) noexcept;
-  SmoothFloat(const SmoothFloat& other) noexcept;
+  SmoothFloat(float agility, float start) noexcept : agility(agility), current(start), target(start) {
+  }
 
-  SmoothFloat& operator=(const SmoothFloat& other) noexcept;
-  SmoothFloat& operator=(float v) noexcept;
-  SmoothFloat& operator+=(float v) noexcept;
-  SmoothFloat& operator-=(float v) noexcept;
+  SmoothFloat(const SmoothFloat& other) noexcept {
+    *this = other;
+  }
 
-  operator float() const;
+  inline SmoothFloat& operator=(const SmoothFloat& other) noexcept {
+    agility = other.agility;
+    current = other.current;
+    target = other.target;
+    return *this;
+  }
+
+  inline SmoothFloat& operator=(float v) noexcept {
+    target = v;
+    return *this;
+  }
+
+  inline SmoothFloat& operator+=(float v) noexcept {
+    target += v;
+    return *this;
+  }
+
+  inline SmoothFloat& operator-=(float v) noexcept {
+    target -= v;
+    return *this;
+  }
+
+  inline operator float() const {
+    return current;
+  }
 };
 
-// TODO these need ot be inlined
 void force(SmoothFloat&);
 void force(SmoothFloat&, float value);
 void update(SmoothFloat&, float dt);
+
+inline void force(SmoothFloat& v) {
+  force(v, v.target);
+}
+
+inline void force(SmoothFloat& v, float value) {
+  v.current = value;
+  v.target = value;
+}
+
+inline void update(SmoothFloat& v, float dt) {
+  v.current += (v.target - v.current) * v.agility * dt;
+}
 
 }  // namespace uinta
 
--- src/math.cpp
+++ src/math.cpp
@@ -1,3 +1,1 @@
 #include "./math/direction.cpp"
-#include "./math/smooth_float.cpp"
-#include "./math/smooth_vec3.cpp"
