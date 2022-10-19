28454eab58022ee1ab06a08972490f5705a5b0e8
Ben <benjamin.s.record@gmail.com>
2022-10-18 23:16:28 -0600

    Smoothing operators inlined

--- include/uinta/math/smooth_vec3.hpp
+++ include/uinta/math/smooth_vec3.hpp
@@ -1,7 +1,7 @@
 #ifndef UINTA_MATH_SMOOTH_VEC3_HPP
 #define UINTA_MATH_SMOOTH_VEC3_HPP
 
-#include <glm/fwd.hpp>
+#include <glm/vec3.hpp>
 #include <uinta/math/smooth_float.hpp>
 
 namespace uinta {
@@ -11,20 +11,57 @@ struct SmoothVec3 {
   SmoothFloat y;
   SmoothFloat z;
 
-  SmoothVec3(float agility, float x = 0, float y = 0, float z = 0);
-  SmoothVec3(float agility, const glm::vec3&);
+  explicit SmoothVec3(float agility, float x = 0, float y = 0, float z = 0) : x({agility, x}), y({agility, y}), z({agility, z}) {
+  }
 
-  SmoothVec3& operator+=(const glm::vec3&);
-  SmoothVec3& operator-=(const glm::vec3&);
-  SmoothVec3& operator=(const glm::vec3&);
+  SmoothVec3(float agility, const glm::vec3& v) : x({agility, v.x}), y({agility, v.y}), z({agility, v.z}) {
+  }
 
-  operator glm::vec3() const;
+  inline SmoothVec3& operator+=(const glm::vec3& v) {
+    x += v.x;
+    y += v.y;
+    z += v.z;
+    return *this;
+  }
+
+  inline SmoothVec3& operator-=(const glm::vec3& v) {
+    x -= v.x;
+    y -= v.y;
+    z -= v.z;
+    return *this;
+  }
+
+  inline SmoothVec3& operator=(const glm::vec3& v) {
+    x = v.x;
+    y = v.y;
+    z = v.z;
+    return *this;
+  }
+
+  inline operator glm::vec3() const {
+    return {x, y, z};
+  }
 };
 
-// TODO these need ot be inlined
 void update(SmoothVec3&, float);
 void force(uinta::SmoothVec3&);
 void force(uinta::SmoothVec3&, const glm::vec3&);
+
+inline void update(uinta::SmoothVec3& v, float dt) {
+  update(v.x, dt);
+  update(v.y, dt);
+  update(v.z, dt);
+}
+
+inline void forceSmoothVec3(uinta::SmoothVec3& v) {
+  force(v, {v.x.target, v.y.target, v.z.target});
+}
+
+inline void forceSmoothVec3(uinta::SmoothVec3& v, const glm::vec3& value) {
+  force(v.x, value.x);
+  force(v.y, value.y);
+  force(v.z, value.z);
+}
 
 }  // namespace uinta
 
--- src/math.cpp
+++ src/math.cpp
@@ -1,4 +1,1 @@
 #include "./math/direction.cpp"
-#include "./math/running_avg.cpp"
-#include "./math/smooth_float.cpp"
-#include "./math/smooth_vec3.cpp"
