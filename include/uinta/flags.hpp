fe35962a39899d8a43b43d8d42ea14d42ded5f1b
Ben <benjamin.s.record@gmail.com>
2022-10-22 15:15:13 -0600

    Flags system

---
+++ include/uinta/flags.hpp
@@ -0,0 +1,17 @@
+#ifndef UINTA_FLAGS_HPP
+#define UINTA_FLAGS_HPP
+
+// TODO these should be in <uinta/fwd.hpp>
+using flag_t = unsigned int;
+using flags_t = unsigned int;
+
+inline bool isFlagSet(const flag_t mask, const flags_t flags) {
+  return flags & mask;
+}
+
+inline void setFlag(const flag_t mask, const bool state, flags_t& flags) {
+  flags &= ~mask;
+  if (state) flags |= mask;
+}
+
+#endif  // UINTA_RUNNER_FLAGS_HPP
