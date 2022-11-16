1687e9cc86660b926fa98d146696640abc27be0a
Ben <benjamin.s.record@gmail.com>
2022-11-15 18:44:23 -0700

    `uinta/fwd.hpp`

---
+++ include/uinta/fwd.hpp
@@ -0,0 +1,34 @@
+#ifndef UINTA_FWD_HPP
+#define UINTA_FWD_HPP
+
+#include <uinta/utils/types.h>
+
+#include <uinta/flags.hpp>
+#include <uinta/utils/macros.hpp>
+
+namespace uinta {
+
+struct BasicCamera;
+struct Display;
+struct InputState;
+struct MemoryLink;
+struct MeshAttrib;
+struct MetricsController;
+struct Quad;
+struct RunnerState;
+struct RunningAvg;
+struct SmoothFloat;
+struct SmoothVec3;
+struct StaticCamera;
+struct TargetCamera;
+struct Vao;
+struct Vbo;
+struct VertexAttrib;
+
+class CartesianGrid;
+class FileManager;
+class Runner;
+
+}  // namespace uinta
+
+#endif  // UINTA_FWD_HPP
