d651e3fbf140c647852cdfa026f6e6b129ccf1f2
Ben <benjamin.s.record@gmail.com>
2022-11-22 21:49:46 -0700

    Camera configuration

---
+++ include/uinta/camera/camera_config.hpp
@@ -0,0 +1,54 @@
+#ifndef UINTA_CAMERA_CONFIG_HPP
+#define UINTA_CAMERA_CONFIG_HPP
+
+#include <uinta/input/keys.hpp>
+
+namespace uinta {
+
+const auto CAMERA_DIST_LIMIT = 1 << 0;
+const auto CAMERA_PITCH_LIMIT = 1 << 1;
+
+struct CameraConfig {
+  flags_t flags = CAMERA_DIST_LIMIT | CAMERA_PITCH_LIMIT;
+
+  float fov = 45;
+
+  float nearPlane = 0.07;
+  float farPlane = 50;
+
+  float translateSpeedKeyboard = 4;
+  float translateSpeedMouse = 0.5;
+  input_key_t translateBackward = KEY_D;
+  input_key_t translateForward = KEY_E;
+  input_key_t translateLeft = KEY_S;
+  input_key_t translateMouse = MOUSE_BUTTON_LEFT;
+  input_key_t translateRight = KEY_F;
+
+  float pitchMax = 90;
+  float pitchMin = 0.5;
+  float pitchSpeedKeyboard = 90;
+  float pitchSpeedMouse = 10;
+  input_key_t pitchDown = KEY_A;
+  input_key_t pitchMouse = MOUSE_BUTTON_RIGHT;
+  input_key_t pitchUp = KEY_Q;
+
+  float distMax = 30;
+  float distMin = 0.2;
+  float distSpeedKeyboard = 6;
+  float distSpeedMouse = 60;
+  input_key_t distDown = KEY_V;
+  input_key_t distUp = KEY_C;
+
+  float angleSpeedKeyboard = 90;
+  float angleSpeedMouse = 10;
+  input_key_t angleLeft = KEY_W;
+  input_key_t angleMouse = MOUSE_BUTTON_RIGHT;
+  input_key_t angleRight = KEY_R;
+
+  CameraConfig() {
+  }
+};
+
+}  // namespace uinta
+
+#endif  // UINTA_CAMERA_CONFIG_HPP
--- include/uinta/camera/target_camera.hpp
+++ include/uinta/camera/target_camera.hpp
@@ -1,7 +1,9 @@
 #ifndef UINTA_TARGET_CAMERA_HPP
 #define UINTA_TARGET_CAMERA_HPP
 
+#include <glm/fwd.hpp>
 #include <glm/vec3.hpp>
+#include <uinta/camera/camera_config.hpp>
 #include <uinta/math/smooth_float.hpp>
 #include <uinta/math/smooth_vec3.hpp>
 
@@ -18,9 +20,11 @@ struct TargetCamera {
   SmoothFloat pitch{8, 0};
   float vertOffset{0};
 
+  CameraConfig config;
+
   TargetCamera() = default;
 
-  TargetCamera(const TargetCamera& other) {
+  TargetCamera(const TargetCamera& other, const CameraConfig config = CameraConfig()) {
     *this = other;
   }
 
