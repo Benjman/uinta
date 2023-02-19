3747dc50117c3d22710a768ce19b3512e9f92320
Ben <benjamin.s.record@gmail.com>
2023-02-19 12:07:07 -0700

    Initial terrain

---
+++ include/uinta/utils/generators.hpp
@@ -0,0 +1,12 @@
+#ifndef UINTA_GENERATORS_HPP
+#define UINTA_GENERATORS_HPP
+
+#include <uinta/utils/types.h>
+
+namespace uinta {
+
+void generateTerrain(float* const vertices, uint* const indices);
+
+}  // namespace uinta
+
+#endif  // UINTA_GENERATORS_HPP
