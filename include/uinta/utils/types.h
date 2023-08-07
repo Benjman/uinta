5cd39af150983ee3e4d70c933c6f50014242d284
Ben <benjamin.s.record@gmail.com>
2023-08-07 13:53:29 -0600

    std int types

--- include/uinta/utils/types.h
+++ include/uinta/utils/types.h
@@ -1,16 +1,18 @@
 #ifndef UINTA_TYPES_H
 #define UINTA_TYPES_H
 
-using uint8 = unsigned char;
-using uint16 = unsigned short;
-using uint32 = unsigned int;
-using uint64 = unsigned long;
+#include <cstdint>
+
+using uint8 = std::uint8_t;
+using uint16 = std::uint16_t;
+using uint32 = std::uint32_t;
+using uint64 = std::uint64_t;
 using uint = uint32;
 
-using int8 = char;
-using int16 = short;
-using int32 = int;
-using int64 = long;
+using int8 = std::int8_t;
+using int16 = std::int16_t;
+using int32 = std::int32_t;
+using int64 = std::int64_t;
 
 using file_size_t = uint;
 using file_t = uint;
