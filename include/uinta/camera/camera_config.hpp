696d10886a92526ea83bf188a6467282599e2db5
Ben <benjamin.s.record@gmail.com>
2023-08-17 11:09:39 -0600

    Updated types

--- include/uinta/camera/camera_config.hpp
+++ include/uinta/camera/camera_config.hpp
@@ -5,44 +5,44 @@
 
 namespace uinta {
 
-const auto CAMERA_DIST_LIMIT = 1 << 0;
-const auto CAMERA_PITCH_LIMIT = 1 << 1;
+const flag_t CAMERA_DIST_LIMIT = 1 << 0;
+const flag_t CAMERA_PITCH_LIMIT = 1 << 1;
 
 struct CameraConfig {
   flags_t flags = CAMERA_DIST_LIMIT | CAMERA_PITCH_LIMIT;
 
-  float fov = 45;
+  f32 fov = 45;
 
-  float nearPlane = 0.07;
-  float farPlane = 200;
+  f32 nearPlane = 0.07;
+  f32 farPlane = 200;
 
-  float translateSpeedKeyboard = 4;
-  float translateSpeedMouse = 0.5;
-  float translationSpeedDistFactor = .45;  // Scales translation speed as a factor proportional to distance
-  float translationSpeedDistFactorMin = .15;
+  f32 translateSpeedKeyboard = 4;
+  f32 translateSpeedMouse = 0.5;
+  f32 translationSpeedDistFactor = .45;  // Scales translation speed as a factor proportional to distance
+  f32 translationSpeedDistFactorMin = .15;
   input_key_t translateBackward = KEY_D;
   input_key_t translateForward = KEY_E;
   input_key_t translateLeft = KEY_S;
   input_key_t translateMouse = MOUSE_BUTTON_LEFT;
   input_key_t translateRight = KEY_F;
 
-  float pitchMax = 90;
-  float pitchMin = 0.5;
-  float pitchSpeedKeyboard = 90;
-  float pitchSpeedMouse = 10;
+  f32 pitchMax = 90;
+  f32 pitchMin = 0.5;
+  f32 pitchSpeedKeyboard = 90;
+  f32 pitchSpeedMouse = 10;
   input_key_t pitchDown = KEY_A;
   input_key_t pitchMouse = MOUSE_BUTTON_RIGHT;
   input_key_t pitchUp = KEY_Q;
 
-  float distMax = 30;
-  float distMin = 0.2;
-  float distSpeedKeyboard = 6;
-  float distSpeedMouse = 60;
+  f32 distMax = 30;
+  f32 distMin = 0.2;
+  f32 distSpeedKeyboard = 6;
+  f32 distSpeedMouse = 60;
   input_key_t distDown = KEY_V;
   input_key_t distUp = KEY_C;
 
-  float angleSpeedKeyboard = 90;
-  float angleSpeedMouse = 10;
+  f32 angleSpeedKeyboard = 90;
+  f32 angleSpeedMouse = 10;
   input_key_t angleLeft = KEY_W;
   input_key_t angleMouse = MOUSE_BUTTON_RIGHT;
   input_key_t angleRight = KEY_R;
