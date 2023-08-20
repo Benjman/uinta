4ff5987dc2379196c7be77977394fcd55b84fa98
Ben <benjamin.s.record@gmail.com>
2023-08-20 13:04:08 -0600

    Compartmentalized scene UI

--- app/include/uinta/glfw/ui/scene.cpp
+++ app/include/uinta/glfw/ui/scene.cpp
@@ -1,28 +1,11 @@
 #include <uinta/glfw/imgui.h>
-#include <uinta/glfw/ui/limits.h>
 
-#include <glm/gtc/type_ptr.hpp>
-#include <uinta/logging.hpp>
 #include <uinta/runner.hpp>
 
 namespace uinta {
 
 inline void scene(Runner& runner) {
   if (!ImGui::CollapsingHeader("Scene")) return;
-  if (ImGui::TreeNode("Lighting")) {
-    static bool hasInitialized = false;
-    static f32 diffuseDirBuffer[3];
-    const auto& light = runner.scene.getDiffuseLight();
-    if (!hasInitialized) {
-      memcpy(diffuseDirBuffer, glm::value_ptr(light.direction), 3 * sizeof(typeof(light.direction.x)));
-      hasInitialized = true;
-    }
-    if (ImGui::SliderFloat3("Diffuse lighting", diffuseDirBuffer, -limits.twenty, limits.twenty))
-      runner.scene.updateDiffuseLight({{diffuseDirBuffer[0], diffuseDirBuffer[1], diffuseDirBuffer[2]}});
-    ImGui::Text("normalized(%+.2f %+.2f %+.2f)", light.direction.x, light.direction.y, light.direction.z);
-    ImGui::TreePop();
-    ImGui::Separator();
-  }
 }
 
 }  // namespace uinta
