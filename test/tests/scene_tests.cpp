0fe033b4e587b1cb695bb2dcd17569284e4e8f55
Ben <benjamin.s.record@gmail.com>
2023-09-16 21:03:29 -0600

    Fixed tests not compiling 🙃

--- test/tests/scene_tests.cpp
+++ test/tests/scene_tests.cpp
@@ -1,5 +1,7 @@
 #include <gtest/gtest.h>
 
+#include "./utils/test_runner.hpp"
+
 #define private public
 #define protected public
 #include <uinta/scene/scene.hpp>
@@ -7,7 +9,8 @@
 using namespace uinta;
 
 TEST(SceneTest, null_renderer) {
+  TestRunner runner;
   SceneDependencies dependencies;
   dependencies.renderer = nullptr;
-  ASSERT_DEATH({ Scene(std::move(dependencies)); }, "Renderer must be initialized") << "Application was expected to die.";
+  ASSERT_DEATH({ Scene(runner, std::move(dependencies)); }, "Renderer must be initialized") << "Application was expected to die.";
 }
