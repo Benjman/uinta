1be0cf69a81a8cf36ad781768c9cf5dfb64dbc07
Ben <benjamin.s.record@gmail.com>
2023-09-16 21:04:14 -0600

    Removing noise from scene shader

--- res/shader/scene.fs
+++ res/shader/scene.fs
@@ -9,17 +9,7 @@ out vec4 out_color;
 uniform vec3 u_lightDir = vec3(0, -1, 0);
 uniform vec3 u_lightColor = vec3(1);
 
-// TODO: Move me to the terrain shader once implemented
-float noise(vec2 st) {
-    vec2 pos = floor(st);
-    vec2 frac = fract(st);
-    float randomValue = fract(sin(dot(pos, vec2(12.9898, 78.233))) * 43758.5453);
-    return mix(randomValue, 1.0 - randomValue, frac.x * frac.y);
-}
-
 void main() {
-  float n = noise(pass_world_pos.xz * 10.0);
-
   // ambient
   float ambientStrength = 0.25;
   vec3 ambient = ambientStrength * u_lightColor;
@@ -31,5 +21,5 @@ void main() {
 
   // Combine ambient and diffuse lighting with color, then blend with noise
   vec3 color = (ambient + diffuse) * pass_color;
-  out_color = vec4(mix(color, color * n, 0.1), 1); // Blending with noise here
+  out_color = vec4(mix(color, color, 0.1), 1); // Blending with noise here
 }
