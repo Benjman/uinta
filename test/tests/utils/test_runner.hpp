e7a813fa3d2ba7d947e2ac4c03e4a0f43493901e
Ben <benjamin.s.record@gmail.com>
2023-09-18 10:17:35 -0600

    Lambdas for test methods calling

--- test/tests/utils/test_runner.hpp
+++ test/tests/utils/test_runner.hpp
@@ -18,30 +18,40 @@ class TestRunner : public Runner {
 
   bool createOpenGLContext_called = false;
   uinta_error_code createOpenGLContext_ec = SUCCESS_EC;
+  std::function<void()> m_on_createOpenGLContext;
   virtual uinta_error_code createOpenGLContext() override {
+    if (m_on_createOpenGLContext) m_on_createOpenGLContext();
     createOpenGLContext_called = true;
     return createOpenGLContext_ec;
   }
 
   f32 rt;
   static constexpr f32 rt_delta = 0.0167;
+  std::function<void()> m_on_runtime = {};
   virtual f64 runtime() override {
+    if (m_on_runtime) m_on_runtime();
     return rt += rt_delta;
   }
 
   bool pollInput_called = false;
+  std::function<void()> m_on_pollInput;
   virtual void pollInput() override {
+    if (m_on_pollInput) m_on_pollInput();
     pollInput_called = true;
   }
 
   bool swapBuffers_called = false;
+  std::function<void()> m_on_swapBuffers;
   virtual void swapBuffers() override {
+    if (m_on_swapBuffers) m_on_swapBuffers();
     swapBuffers_called = true;
   }
 
   bool doInit_called = false;
   uinta_error_code doInit_ec = SUCCESS_EC;
+  std::function<void()> m_on_doInit;
   virtual uinta_error_code doInit() override {
+    if (m_on_doInit) m_on_doInit();
     doInit_called = true;
     return doInit_ec;
   }
