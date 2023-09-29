2313f27fdaf17452a9c3e7290ba80edac63c3327
Ben <benjamin.s.record@gmail.com>
2023-09-28 21:43:24 -0600

    Moving `Light` to be a component

--- include/uinta/component/light.hpp
+++ include/uinta/component/light.hpp
@@ -1,6 +1,7 @@
 #ifndef UINTA_SCENE_LIGHT_HPP
 #define UINTA_SCENE_LIGHT_HPP
 
+#include <glm/geometric.hpp>
 #include <glm/vec3.hpp>
 
 namespace uinta {
@@ -10,9 +11,9 @@ struct Light {
   glm::vec3 pos;
   glm::vec3 color;
 
-  Light(const glm::vec3& direction, const glm::vec3& pos = {0, 0, 0}, const glm::vec3& color = {1, 1, 1}) noexcept;
-
-  Light(const Light& other) noexcept;
+  Light(const glm::vec3& direction, const glm::vec3& pos, const glm::vec3& color) noexcept
+      : direction(glm::normalize(direction)), pos(pos), color(color) {
+  }
 
   Light& operator=(const Light& other) noexcept;
 };
