4094170f204e72e265732b9123ff0a6add791a48
Ben <benjamin.s.record@gmail.com>
2023-10-07 18:57:06 -0600

    Inlined perlin

--- include/uinta/math/perlin.hpp
+++ include/uinta/math/perlin.hpp
@@ -18,9 +18,14 @@ namespace uinta {
  * @param octaves The successive layers of noise functions that are combined to create more complex, finer-grained patterns
  * @param offset The offset to apply to the Perlin noise sampling.
  */
-void perlinNoise(Buffer2d<f32>& buffer, const siv::PerlinNoise& perlin, f32 frequency, i32 octaves,
-                 const glm::ivec2& offset = {});
-
+inline void perlinNoise(Buffer2d<f32>& buffer, const siv::PerlinNoise& perlin, f32 frequency, i32 octaves,
+                        const glm::ivec2& offset = {}) {
+  const auto fx = frequency / buffer.size.x;
+  const auto fy = frequency / buffer.size.y;
+  for (auto y = 0; y < buffer.size.y; ++y)
+    for (auto x = 0; x < buffer.size.x; ++x)
+      buffer[y * buffer.size.x + x] = perlin.octave2D_01(((x + offset.x) * fx), ((y + offset.y) * fy), octaves);
+}
 }  // namespace uinta
 
 #endif /* ifndef UINTA_MATH_PERLIN_HPP */
