ab1b002e1282397f1cdd1cbe2bdca1e43a6fe095
Ben <benjamin.s.record@gmail.com>
2023-10-23 14:55:09 -0600

    Formatting

--- include/uinta/fwd.hpp
+++ include/uinta/fwd.hpp
@@ -1,14 +1,8 @@
 #ifndef UINTA_FWD_HPP
 #define UINTA_FWD_HPP
 
-#include <uinta/flags.h>
 #include <uinta/types.h>
-#include <uinta/utils/macros.h>
-
-#include <system_error>
-
 namespace uinta {
-
 class FileManager;
 class GridScene;
 class Runner;
@@ -18,20 +12,24 @@ class UiElement;
 class UiManager;
 class UiPane;
 class UintaException;
-
 struct Event;
 struct InputState;
 struct Light;
 struct RunnerState;
 struct Window;
-
 using event_t = u32;
 using input_key_t = u32;
 using mouse_button_t = u32;
 using subscription_t = u32;
-using uinta_error_code = std::error_code;
 using uinta_error_code_t = i32;
-
 }  // namespace uinta
+
+#include <system_error>
+namespace uinta {
+using uinta_error_code = std::error_code;
+}
+
+#include <uinta/flags.h>
+#include <uinta/utils/macros.h>
 
 #endif  // UINTA_FWD_HPP
