064b97c63b82a46fde803269901e149241abb6d8
Ben <benjamin.s.record@gmail.com>
2024-02-06 21:08:40 -0700

    Hello Uinta Engine

---
+++ src/engine/include/uinta/engine/engine.h
@@ -0,0 +1,23 @@
+#ifndef SRC_ENGINE_INCLUDE_UINTA_ENGINE_ENGINE_H_
+#define SRC_ENGINE_INCLUDE_UINTA_ENGINE_ENGINE_H_
+
+#include <string>
+
+namespace uinta {
+
+class Engine {
+ public:
+  Engine() noexcept = default;
+
+  ~Engine() noexcept = default;
+  Engine(const Engine&) noexcept = delete;
+  Engine& operator=(const Engine&) noexcept = delete;
+  Engine(const Engine&&) noexcept = delete;
+  Engine& operator=(const Engine&&) noexcept = delete;
+
+  const std::string Message() const noexcept;
+};
+
+}  // namespace uinta
+
+#endif  // SRC_ENGINE_INCLUDE_UINTA_ENGINE_ENGINE_H_
---
+++ src/engine/src/engine.cc
@@ -0,0 +1,9 @@
+#include "uinta/engine/engine.h"
+
+namespace uinta {
+
+const std::string Engine::Message() const noexcept {
+  return "Hello from Uinta Engine\n";
+}
+
+}  // namespace uinta
