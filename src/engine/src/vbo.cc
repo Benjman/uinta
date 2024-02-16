67be4d49495b69be7d24a2f5ec3aa88de3a552dc
Ben <benjamin.s.record@gmail.com>
2024-02-15 20:28:36 -0700

    `Vbo` impl

---
+++ src/engine/src/vbo.cc
@@ -0,0 +1,19 @@
+#include "uinta/vbo.h"
+
+namespace uinta {
+
+VboGuard::VboGuard(const Vbo* vbo, bool isActive, const OpenGLApi* gl) noexcept
+    : vbo_(vbo), gl_(gl) {
+  if (isActive) activate();
+}
+
+VboGuard::~VboGuard() noexcept {
+  if (isActive_) gl_->bindBuffer(vbo_->target_, 0);
+}
+
+void VboGuard::activate() noexcept {
+  vbo_->gl_->bindBuffer(vbo_->target_, vbo_->id_);
+  isActive_ = true;
+}
+
+}  // namespace uinta
