75086533fa655fdc4d72ace11e91504fe63208aa
Ben <benjamin.s.record@gmail.com>
2024-02-20 18:16:03 -0700

    demo: Hello cube!

--- src/app/src/demo.h
+++ src/app/src/demo.h
@@ -3,12 +3,10 @@
 
 #include <string>
 
-#include "absl/log/log.h"
 #include "uinta/engine/engine.h"
 #include "uinta/scene.h"
 #include "uinta/shader.h"
 #include "uinta/shaders/primitive.h"
-#include "uinta/texture.h"
 #include "uinta/vao.h"
 #include "uinta/vbo.h"
 
@@ -20,58 +18,49 @@ class DemoScene : public Scene {
  public:
   explicit DemoScene(Scene* parent, Engine* engine) noexcept
       : Scene(parent, engine, Layer::Simulation),
-        texture_(GL_TEXTURE_2D, 0, 0, 0, 0, 0, engine->gl()),
         shader_(engine->gl()),
         vbo_(GL_ARRAY_BUFFER, 0, engine->gl()),
         gl_(engine->gl()) {
     addSystem<PrimitiveShaderManager>(&shader_, engine->dispatchers());
 
-    std::array<Vertex, 4> vertices = {
-        Vertex({0.5f, 0.5f, -1.0}, {0.0, 0.0, 1.0}, {1.0, 1.0, 1.0},
-               {1.0f, 1.0f}),
-        Vertex({0.5f, -0.5f, -1.0}, {0.0, 0.0, 1.0}, {1.0, 1.0, 1.0},
-               {1.0f, 0.0f}),
-        Vertex({-0.5f, -0.5f, -1.0}, {0.0, 0.0, 1.0}, {1.0, 1.0, 1.0},
-               {0.0f, 0.0f}),
-        Vertex({-0.5f, 0.5f, -1.0}, {0.0, 0.0, 1.0}, {1.0, 1.0, 1.0},
-               {0.0f, 1.0f}),
-    };
+    size_t idxOffset = 0;
+    auto mesh = Mesh::Cube(&idxOffset);
+
     VboGuard vbg(&vbo_);
-    vbo_.bufferData(vertices.data(), vertices.size() * sizeof(f32),
+    vbo_.bufferData(mesh.vertices().data(),
+                    mesh.vertices().size() * Vertex::ElementCount * sizeof(f32),
                     GL_STATIC_DRAW);
-    std::array<u32, 6> indices = {0, 1, 3, 1, 2, 3};
-    Mesh mesh(vertices, indices);
 
-    auto vsize = mesh.vertices().size() * Vertex::ElementCount * sizeof(f32);
     VaoGuard vag(&vao_);
-    vbo_.bufferData(mesh.vertices().data(), vsize, GL_STATIC_DRAW);
     vao_.ebo(mesh.elements());
-
-    if (auto status = texture_.fromFile("wall.jpg"); !status.ok()) {
-      LOG(FATAL) << status.message();
-      return;
-    }
+    indexCount_ = mesh.elements().size();
 
     ShaderGuard sg(&shader_);
     shader_.linkAttributes(&vao_);
-    shader_.lightDir = glm::vec3(0, 0, -1);
-    shader_.uv();
+    shader_.view = glm::translate(glm::mat4(1), glm::vec3(0, 0, -1));
   }
 
   std::string name() const noexcept override { return "Demo scene"; }
 
-  void render(const EngineState&) noexcept override {
-    ShaderGuard shaderGuard(&shader_);
-    VaoGuard vaoGuard(&vao_);
-    TextureGuard textureGuard(&texture_);
-    gl_->drawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+  void render(const EngineState& state) noexcept override {
+    DepthTestGuard dtg;
+    CullFaceGuard cfg;
+    ShaderGuard sg(&shader_);
+    VaoGuard vg(&vao_);
+
+    auto model = glm::rotate(
+        glm::mat4(1), glm::radians(static_cast<f32>(state.runtime()) * 30),
+        glm::normalize(glm::vec3(.25, 1, 0)));
+    shader_.model = glm::scale(model, glm::vec3(0.5));
+
+    glDrawElements(GL_TRIANGLES, indexCount_, GL_UNSIGNED_INT, 0);
   }
 
  private:
-  Texture texture_;
   PrimitiveShader shader_;
   Vao vao_;
   Vbo vbo_;
+  size_t indexCount_;
   const OpenGLApi* gl_;
 };
 
