e0064b2bddf565c5e6d1901208e2dc3932fa6716
Ben <benjamin.s.record@gmail.com>
2024-02-22 21:33:37 -0700

    math utils

---
+++ src/engine/include/uinta/math/defs.h
@@ -0,0 +1,14 @@
+#ifndef SRC_ENGINE_INCLUDE_UINTA_MATH_DEFS_H_
+#define SRC_ENGINE_INCLUDE_UINTA_MATH_DEFS_H_
+
+#include <limits>
+
+#include "uinta/types.h"
+
+namespace uinta {
+
+constexpr f32 epsilon_f = std::numeric_limits<f32>::epsilon();
+
+}  // namespace uinta
+
+#endif  // SRC_ENGINE_INCLUDE_UINTA_MATH_DEFS_H_
---
+++ src/engine/include/uinta/math/smooth_float.h
@@ -0,0 +1,87 @@
+#ifndef SRC_ENGINE_INCLUDE_UINTA_MATH_SMOOTH_FLOAT_H_
+#define SRC_ENGINE_INCLUDE_UINTA_MATH_SMOOTH_FLOAT_H_
+
+#include <cstdlib>  // for std::abs
+
+#include "uinta/math/defs.h"
+
+namespace uinta {
+
+class SmoothFloat {
+ public:
+  explicit SmoothFloat(f32 agility, f32 start = 0) noexcept
+      : agility_(agility), current_(start), target_(start) {}
+
+  void force() noexcept { force(target_); }
+
+  void force(f32 value) noexcept {
+    current_ = value;
+    target_ = value;
+  }
+
+  void update(f32 dt) noexcept {
+    current_ += (target_ - current_) * agility_ * dt;
+  }
+
+  SmoothFloat& operator=(f32 v) noexcept {
+    target_ = v;
+    return *this;
+  }
+
+  SmoothFloat& operator+=(f32 v) noexcept {
+    target_ += v;
+    return *this;
+  }
+
+  SmoothFloat& operator-=(f32 v) noexcept {
+    target_ -= v;
+    return *this;
+  }
+
+  bool operator==(const SmoothFloat& other) const noexcept {
+    return std::abs(current_ - other.current_) <= epsilon_f;
+  }
+
+  bool operator!=(const SmoothFloat& other) const noexcept {
+    return std::abs(current_ - other.current_) != epsilon_f;
+  }
+
+  bool operator>(const SmoothFloat& other) const noexcept {
+    return std::abs(current_ - other.current_) > epsilon_f;
+  }
+
+  bool operator<(const SmoothFloat& other) const noexcept {
+    return std::abs(current_ - other.current_) < epsilon_f;
+  }
+
+  bool operator>=(const SmoothFloat& other) const noexcept {
+    return std::abs(current_ - other.current_) >= epsilon_f;
+  }
+
+  bool operator<=(const SmoothFloat& other) const noexcept {
+    return std::abs(current_ - other.current_) <= epsilon_f;
+  }
+
+  operator f32() const noexcept { return current_; }
+
+  f32 agility() const noexcept { return agility_; }
+
+  void agility(f32 v) noexcept { agility_ = v; }
+
+  f32 current() const noexcept { return current_; }
+
+  void current(f32 v) noexcept { current_ = v; }
+
+  f32 target() const noexcept { return target_; }
+
+  void target(f32 v) noexcept { target_ = v; }
+
+ private:
+  f32 agility_;
+  f32 current_;
+  f32 target_;
+};
+
+}  // namespace uinta
+
+#endif  // SRC_ENGINE_INCLUDE_UINTA_MATH_SMOOTH_FLOAT_H_
