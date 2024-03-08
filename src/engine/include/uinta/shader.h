e81fccce1cebbc2132c5d05c81af947f5eecd592
Ben <benjamin.s.record@gmail.com>
2024-03-08 13:04:16 -0700

    feat: `Shader`

---
+++ src/engine/include/uinta/shader.h
@@ -0,0 +1,55 @@
+#ifndef SRC_ENGINE_INCLUDE_UINTA_SHADER_H_
+#define SRC_ENGINE_INCLUDE_UINTA_SHADER_H_
+
+#include <memory>
+#include <string>
+#include <unordered_map>
+
+#include "uinta/file.h"
+#include "uinta/gl.h"
+
+namespace uinta {
+
+class Shader {
+ public:
+  Shader(const std::unordered_map<GLenum, std::string> stagePaths,
+         const OpenGLApi*,
+         std::shared_ptr<FileSystem> =
+             std::make_shared<FileSystemImpl>()) noexcept;
+
+  ~Shader() noexcept;
+
+  Shader(const Shader&) noexcept = delete;
+  Shader& operator=(const Shader&) noexcept = delete;
+  Shader(Shader&&) noexcept = delete;
+  Shader& operator=(Shader&&) noexcept = delete;
+
+  GLuint id() const noexcept { return id_; }
+
+  const OpenGLApi* gl() const noexcept { return gl_; }
+
+ private:
+  GLuint id_ = 0;
+  const OpenGLApi* gl_;
+};
+
+class ShaderGuard {
+ public:
+  explicit ShaderGuard(const Shader* shader) noexcept : gl_(shader->gl()) {
+    gl_->useProgram(shader->id());
+  }
+
+  ~ShaderGuard() noexcept { gl_->useProgram(0); }
+
+  ShaderGuard(const ShaderGuard&) noexcept = delete;
+  ShaderGuard& operator=(const ShaderGuard&) noexcept = delete;
+  ShaderGuard(ShaderGuard&&) noexcept = delete;
+  ShaderGuard& operator=(ShaderGuard&&) noexcept = delete;
+
+ protected:
+  const OpenGLApi* gl_;
+};
+
+}  // namespace uinta
+
+#endif  // SRC_ENGINE_INCLUDE_UINTA_SHADER_H_
---
+++ src/engine/src/shader.cc
@@ -0,0 +1,61 @@
+#include "uinta/shader.h"
+
+#include "absl/log/log.h"
+#include "uinta/lib/absl/strings.h"
+
+namespace uinta {
+
+Shader::Shader(const std::unordered_map<GLenum, std::string> stagePaths,
+               const OpenGLApi* gl, std::shared_ptr<FileSystem> fs) noexcept
+    : gl_(gl) {
+  auto id = gl_->createProgram();
+  for (auto& pair : stagePaths) {
+    File file(pair.second, fs);
+    if (!file.status().ok()) {
+      LOG(FATAL) << file.status().message();
+      return;
+    }
+    LOG(INFO) << "Compiling shader: " << file.path();
+    auto src = file.readAll();
+    const GLchar* data = src.data();
+    auto len = static_cast<GLint>(src.size());
+    auto shaderId = gl_->createShader(pair.first);
+    gl_->shaderSource(shaderId, 1, &data, &len);
+    gl_->compileShader(shaderId);
+    GLint isCompiled = 0;
+    gl_->getShaderiv(shaderId, GL_COMPILE_STATUS, &isCompiled);
+    if (!isCompiled) {
+      constexpr size_t kInfoLen = 1024;
+      GLchar info[kInfoLen];
+      GLsizei logSize;
+      gl_->getShaderInfoLog(shaderId, kInfoLen, &logSize, info);
+      gl_->deleteShader(shaderId);
+      LOG(FATAL) << StrFormat("%s: %s", pair.second,
+                              std::string(info, logSize));
+      return;
+    }
+    gl_->attachShader(id, shaderId);
+    gl_->deleteShader(shaderId);
+  }
+
+  gl_->linkProgram(id);
+  GLint success;
+  gl_->getProgramiv(id, GL_LINK_STATUS, &success);
+  if (!success) {
+    constexpr size_t kInfoLen = 1024;
+    GLchar info[kInfoLen];
+    GLsizei logSize;
+    gl_->getProgramInfoLog(id, kInfoLen, &logSize, info);
+    LOG(FATAL) << std::string(info, logSize);
+    return;
+  }
+  id_ = id;
+  LOG(INFO) << "Created shader: " << id_;
+}
+
+Shader::~Shader() noexcept {
+  gl_->deleteProgram(id_);
+  id_ = 0;
+}
+
+}  // namespace uinta
