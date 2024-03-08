95c55bd7390e943d0847fc1522b661481ef8e6c4
Ben <benjamin.s.record@gmail.com>
2024-03-08 13:04:16 -0700

    Shader impl

---
+++ src/engine/src/shader.cc
@@ -0,0 +1,74 @@
+#include "uinta/shader.h"
+
+#include "absl/log/log.h"
+#include "uinta/lib/absl/strings.h"
+
+namespace uinta {
+
+Shader::Shader(const std::unordered_map<GLenum, std::string> stagePaths,
+               const OpenGLApi* gl, std::shared_ptr<FileSystem> fs) noexcept
+    : gl_(gl) {
+  auto id = gl_->createProgram();
+  LOG(INFO) << StrFormat("Creating shader %i:", id);
+  for (auto& pair : stagePaths) {
+    LOG(INFO) << "Compiling shader: " << pair.second;
+    auto file = File(pair.second, fs);
+    if (!file.status().ok()) {
+      LOG(FATAL) << file.status().message();
+      return;
+    }
+    auto src = file.readAll();
+    const GLchar* data = src.data();
+    auto len = static_cast<GLint>(src.size());
+    auto shaderId = gl_->createShader(pair.first);
+    gl_->shaderSource(shaderId, 1, &data, &len);
+    gl_->compileShader(shaderId);
+    GLint isCompiled = 0;
+    gl_->getShaderiv(shaderId, GL_COMPILE_STATUS, &isCompiled);
+    if (!isCompiled) {
+      constexpr size_t kInfoLen = 1024;
+      GLchar info[kInfoLen];
+      GLsizei logSize;
+      gl_->getShaderInfoLog(shaderId, kInfoLen, &logSize, info);
+      gl_->deleteShader(shaderId);
+      LOG(FATAL) << StrFormat("%s: %s", pair.second,
+                              std::string(info, logSize));
+      return;
+    }
+    gl_->attachShader(id, shaderId);
+    gl_->deleteShader(shaderId);
+  }
+
+  gl_->linkProgram(id);
+  GLint success;
+  gl_->getProgramiv(id, GL_LINK_STATUS, &success);
+  if (!success) {
+    constexpr size_t kInfoLen = 1024;
+    GLchar info[kInfoLen];
+    GLsizei logSize;
+    gl_->getProgramInfoLog(id, kInfoLen, &logSize, info);
+    LOG(FATAL) << std::string(info, logSize);
+    return;
+  }
+  id_ = id;
+  LOG(INFO) << StrFormat("Shader %i compiled and linked.", id_);
+}
+
+Shader::~Shader() noexcept {
+  gl_->deleteProgram(id_);
+  id_ = 0;
+}
+
+Shader::Shader(Shader&& other) noexcept : id_(other.id_) { other.id_ = 0; }
+
+Shader& Shader::operator=(Shader&& other) noexcept {
+  if (this != &other) {
+    gl_->deleteProgram(id_);
+    id_ = other.id_;
+
+    other.id_ = 0;
+  }
+  return *this;
+}
+
+}  // namespace uinta
