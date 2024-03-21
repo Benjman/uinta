9bdac839e7793d94a76ecc8dd820d4d9f31cacb2
Ben <benjamin.s.record@gmail.com>
2024-03-21 14:15:59 -0600

    Color palette

---
+++ src/engine/include/uinta/color.h
@@ -0,0 +1,288 @@
+#ifndef SRC_ENGINE_INCLUDE_UINTA_COLOR_H_
+#define SRC_ENGINE_INCLUDE_UINTA_COLOR_H_
+
+#include "glm/ext/vector_float3.hpp"
+
+namespace uinta {
+namespace color {
+
+constexpr glm::vec3 Red50(1.0, 0.921569, 0.933333);
+constexpr glm::vec3 Red100(1.0, 0.803922, 0.823529);
+constexpr glm::vec3 Red200(0.937255, 0.603922, 0.603922);
+constexpr glm::vec3 Red300(0.898039, 0.45098, 0.45098);
+constexpr glm::vec3 Red400(0.937255, 0.32549, 0.313725);
+constexpr glm::vec3 Red500(0.956863, 0.262745, 0.211765);
+constexpr glm::vec3 Red600(0.898039, 0.223529, 0.207843);
+constexpr glm::vec3 Red700(0.827451, 0.184314, 0.184314);
+constexpr glm::vec3 Red800(0.776471, 0.156863, 0.156863);
+constexpr glm::vec3 Red900(0.717647, 0.109804, 0.109804);
+constexpr glm::vec3 RedA100(1.0, 0.541176, 0.501961);
+constexpr glm::vec3 RedA200(1.0, 0.321569, 0.321569);
+constexpr glm::vec3 RedA400(1.0, 0.090196, 0.266667);
+constexpr glm::vec3 RedA700(0.835294, 0.0, 0.0);
+
+constexpr glm::vec3 Pink50(0.988235, 0.894118, 0.92549);
+constexpr glm::vec3 Pink100(0.972549, 0.733333, 0.815686);
+constexpr glm::vec3 Pink200(0.956863, 0.560784, 0.694118);
+constexpr glm::vec3 Pink300(0.941176, 0.384314, 0.572549);
+constexpr glm::vec3 Pink400(0.92549, 0.25098, 0.478431);
+constexpr glm::vec3 Pink500(0.913725, 0.117647, 0.388235);
+constexpr glm::vec3 Pink600(0.847059, 0.105882, 0.376471);
+constexpr glm::vec3 Pink700(0.760784, 0.094118, 0.356863);
+constexpr glm::vec3 Pink800(0.678431, 0.078431, 0.341176);
+constexpr glm::vec3 Pink900(0.533333, 0.054902, 0.309804);
+constexpr glm::vec3 PinkA100(1.0, 0.501961, 0.670588);
+constexpr glm::vec3 PinkA200(1.0, 0.25098, 0.505882);
+constexpr glm::vec3 PinkA400(0.960784, 0.0, 0.341176);
+constexpr glm::vec3 PinkA700(0.772549, 0.066667, 0.384314);
+
+constexpr glm::vec3 Purple50(0.952941, 0.898039, 0.960784);
+constexpr glm::vec3 Purple100(0.882353, 0.745098, 0.905882);
+constexpr glm::vec3 Purple200(0.807843, 0.576471, 0.847059);
+constexpr glm::vec3 Purple300(0.729412, 0.407843, 0.784314);
+constexpr glm::vec3 Purple400(0.670588, 0.278431, 0.737255);
+constexpr glm::vec3 Purple500(0.611765, 0.152941, 0.690196);
+constexpr glm::vec3 Purple600(0.556863, 0.141176, 0.666667);
+constexpr glm::vec3 Purple700(0.482353, 0.121569, 0.635294);
+constexpr glm::vec3 Purple800(0.415686, 0.105882, 0.603922);
+constexpr glm::vec3 Purple900(0.290196, 0.078431, 0.54902);
+constexpr glm::vec3 PurpleA100(0.917647, 0.501961, 0.988235);
+constexpr glm::vec3 PurpleA200(0.878431, 0.25098, 0.984314);
+constexpr glm::vec3 PurpleA400(0.835294, 0.0, 0.976471);
+constexpr glm::vec3 PurpleA700(0.666667, 0.0, 1.0);
+
+constexpr glm::vec3 DeepPurple50(0.929412, 0.905882, 0.964706);
+constexpr glm::vec3 DeepPurple100(0.819608, 0.768627, 0.913725);
+constexpr glm::vec3 DeepPurple200(0.701961, 0.615686, 0.858824);
+constexpr glm::vec3 DeepPurple300(0.584314, 0.458824, 0.803922);
+constexpr glm::vec3 DeepPurple400(0.494118, 0.341176, 0.760784);
+constexpr glm::vec3 DeepPurple500(0.403922, 0.227451, 0.717647);
+constexpr glm::vec3 DeepPurple600(0.368627, 0.207843, 0.694118);
+constexpr glm::vec3 DeepPurple700(0.317647, 0.176471, 0.658824);
+constexpr glm::vec3 DeepPurple800(0.270588, 0.152941, 0.627451);
+constexpr glm::vec3 DeepPurple900(0.192157, 0.105882, 0.572549);
+constexpr glm::vec3 DeepPurpleA100(0.701961, 0.533333, 1.0);
+constexpr glm::vec3 DeepPurpleA200(0.486275, 0.301961, 1.0);
+constexpr glm::vec3 DeepPurpleA400(0.396078, 0.121569, 1.0);
+constexpr glm::vec3 DeepPurpleA700(0.384314, 0.0, 0.917647);
+
+constexpr glm::vec3 Indigo50(0.909804, 0.917647, 0.964706);
+constexpr glm::vec3 Indigo100(0.772549, 0.792157, 0.913725);
+constexpr glm::vec3 Indigo200(0.623529, 0.658824, 0.854902);
+constexpr glm::vec3 Indigo300(0.47451, 0.52549, 0.796078);
+constexpr glm::vec3 Indigo400(0.360784, 0.419608, 0.752941);
+constexpr glm::vec3 Indigo500(0.247059, 0.317647, 0.709804);
+constexpr glm::vec3 Indigo600(0.223529, 0.286275, 0.670588);
+constexpr glm::vec3 Indigo700(0.188235, 0.247059, 0.623529);
+constexpr glm::vec3 Indigo800(0.156863, 0.207843, 0.576471);
+constexpr glm::vec3 Indigo900(0.101961, 0.137255, 0.494118);
+constexpr glm::vec3 IndigoA100(0.54902, 0.619608, 1.0);
+constexpr glm::vec3 IndigoA200(0.32549, 0.427451, 0.996078);
+constexpr glm::vec3 IndigoA400(0.239216, 0.352941, 0.996078);
+constexpr glm::vec3 IndigoA700(0.188235, 0.309804, 0.996078);
+
+constexpr glm::vec3 Blue50(0.890196, 0.94902, 0.992157);
+constexpr glm::vec3 Blue100(0.733333, 0.870588, 0.984314);
+constexpr glm::vec3 Blue200(0.564706, 0.792157, 0.976471);
+constexpr glm::vec3 Blue300(0.392157, 0.709804, 0.964706);
+constexpr glm::vec3 Blue400(0.258824, 0.647059, 0.960784);
+constexpr glm::vec3 Blue500(0.129412, 0.588235, 0.952941);
+constexpr glm::vec3 Blue600(0.117647, 0.533333, 0.898039);
+constexpr glm::vec3 Blue700(0.098039, 0.462745, 0.823529);
+constexpr glm::vec3 Blue800(0.082353, 0.396078, 0.752941);
+constexpr glm::vec3 Blue900(0.05098, 0.278431, 0.631373);
+constexpr glm::vec3 BlueA100(0.509804, 0.694118, 1.0);
+constexpr glm::vec3 BlueA200(0.266667, 0.541176, 1.0);
+constexpr glm::vec3 BlueA400(0.160784, 0.47451, 1.0);
+constexpr glm::vec3 BlueA700(0.160784, 0.384314, 1.0);
+
+constexpr glm::vec3 LightBlue50(0.882353, 0.960784, 0.996078);
+constexpr glm::vec3 LightBlue100(0.701961, 0.898039, 0.988235);
+constexpr glm::vec3 LightBlue200(0.505882, 0.831373, 0.980392);
+constexpr glm::vec3 LightBlue300(0.309804, 0.764706, 0.968627);
+constexpr glm::vec3 LightBlue400(0.160784, 0.713725, 0.964706);
+constexpr glm::vec3 LightBlue500(0.011765, 0.662745, 0.956863);
+constexpr glm::vec3 LightBlue600(0.011765, 0.607843, 0.898039);
+constexpr glm::vec3 LightBlue700(0.007843, 0.533333, 0.819608);
+constexpr glm::vec3 LightBlue800(0.007843, 0.466667, 0.741176);
+constexpr glm::vec3 LightBlue900(0.003922, 0.341176, 0.607843);
+constexpr glm::vec3 LightBlueA100(0.501961, 0.847059, 1.0);
+constexpr glm::vec3 LightBlueA200(0.25098, 0.768627, 1.0);
+constexpr glm::vec3 LightBlueA400(0.0, 0.690196, 1.0);
+constexpr glm::vec3 LightBlueA700(0.0, 0.568627, 0.917647);
+
+constexpr glm::vec3 Cyan50(0.878431, 0.968627, 0.980392);
+constexpr glm::vec3 Cyan100(0.698039, 0.921569, 0.94902);
+constexpr glm::vec3 Cyan200(0.501961, 0.870588, 0.917647);
+constexpr glm::vec3 Cyan300(0.301961, 0.815686, 0.882353);
+constexpr glm::vec3 Cyan400(0.14902, 0.776471, 0.854902);
+constexpr glm::vec3 Cyan500(0.0, 0.737255, 0.831373);
+constexpr glm::vec3 Cyan600(0.0, 0.67451, 0.756863);
+constexpr glm::vec3 Cyan700(0.0, 0.592157, 0.654902);
+constexpr glm::vec3 Cyan800(0.0, 0.513725, 0.560784);
+constexpr glm::vec3 Cyan900(0.0, 0.376471, 0.392157);
+constexpr glm::vec3 CyanA100(0.517647, 1.0, 1.0);
+constexpr glm::vec3 CyanA200(0.094118, 1.0, 1.0);
+constexpr glm::vec3 CyanA400(0.0, 0.898039, 1.0);
+constexpr glm::vec3 CyanA700(0.0, 0.721569, 0.831373);
+
+constexpr glm::vec3 Teal50(0.878431, 0.94902, 0.945098);
+constexpr glm::vec3 Teal100(0.698039, 0.87451, 0.858824);
+constexpr glm::vec3 Teal200(0.501961, 0.796078, 0.768627);
+constexpr glm::vec3 Teal300(0.301961, 0.713725, 0.67451);
+constexpr glm::vec3 Teal400(0.14902, 0.65098, 0.603922);
+constexpr glm::vec3 Teal500(0.0, 0.588235, 0.533333);
+constexpr glm::vec3 Teal600(0.0, 0.537255, 0.482353);
+constexpr glm::vec3 Teal700(0.0, 0.47451, 0.419608);
+constexpr glm::vec3 Teal800(0.0, 0.411765, 0.360784);
+constexpr glm::vec3 Teal900(0.0, 0.301961, 0.25098);
+constexpr glm::vec3 TealA100(0.654902, 1.0, 0.921569);
+constexpr glm::vec3 TealA200(0.392157, 1.0, 0.854902);
+constexpr glm::vec3 TealA400(0.113725, 0.913725, 0.713725);
+constexpr glm::vec3 TealA700(0.0, 0.74902, 0.647059);
+
+constexpr glm::vec3 Green50(0.909804, 0.960784, 0.913725);
+constexpr glm::vec3 Green100(0.784314, 0.901961, 0.788235);
+constexpr glm::vec3 Green200(0.647059, 0.839216, 0.654902);
+constexpr glm::vec3 Green300(0.505882, 0.780392, 0.517647);
+constexpr glm::vec3 Green400(0.4, 0.733333, 0.415686);
+constexpr glm::vec3 Green500(0.298039, 0.686275, 0.313725);
+constexpr glm::vec3 Green600(0.262745, 0.627451, 0.278431);
+constexpr glm::vec3 Green700(0.219608, 0.556863, 0.235294);
+constexpr glm::vec3 Green800(0.180392, 0.490196, 0.196078);
+constexpr glm::vec3 Green900(0.105882, 0.368627, 0.12549);
+constexpr glm::vec3 GreenA100(0.72549, 0.964706, 0.792157);
+constexpr glm::vec3 GreenA200(0.411765, 0.941176, 0.682353);
+constexpr glm::vec3 GreenA400(0.0, 0.901961, 0.462745);
+constexpr glm::vec3 GreenA700(0.0, 0.784314, 0.32549);
+
+constexpr glm::vec3 LightGreen50(0.945098, 0.972549, 0.913725);
+constexpr glm::vec3 LightGreen100(0.862745, 0.929412, 0.784314);
+constexpr glm::vec3 LightGreen200(0.772549, 0.882353, 0.647059);
+constexpr glm::vec3 LightGreen300(0.682353, 0.835294, 0.505882);
+constexpr glm::vec3 LightGreen400(0.611765, 0.8, 0.396078);
+constexpr glm::vec3 LightGreen500(0.545098, 0.764706, 0.290196);
+constexpr glm::vec3 LightGreen600(0.486275, 0.701961, 0.258824);
+constexpr glm::vec3 LightGreen700(0.407843, 0.623529, 0.219608);
+constexpr glm::vec3 LightGreen800(0.333333, 0.545098, 0.184314);
+constexpr glm::vec3 LightGreen900(0.2, 0.411765, 0.117647);
+constexpr glm::vec3 LightGreenA100(0.8, 1.0, 0.564706);
+constexpr glm::vec3 LightGreenA200(0.698039, 1.0, 0.34902);
+constexpr glm::vec3 LightGreenA400(0.462745, 1.0, 0.011765);
+constexpr glm::vec3 LightGreenA700(0.392157, 0.866667, 0.090196);
+
+constexpr glm::vec3 Lime50(0.976471, 0.984314, 0.905882);
+constexpr glm::vec3 Lime100(0.941176, 0.956863, 0.764706);
+constexpr glm::vec3 Lime200(0.901961, 0.933333, 0.611765);
+constexpr glm::vec3 Lime300(0.862745, 0.905882, 0.458824);
+constexpr glm::vec3 Lime400(0.831373, 0.882353, 0.341176);
+constexpr glm::vec3 Lime500(0.803922, 0.862745, 0.223529);
+constexpr glm::vec3 Lime600(0.752941, 0.792157, 0.2);
+constexpr glm::vec3 Lime700(0.686275, 0.705882, 0.168627);
+constexpr glm::vec3 Lime800(0.619608, 0.615686, 0.141176);
+constexpr glm::vec3 Lime900(0.509804, 0.466667, 0.090196);
+constexpr glm::vec3 LimeA100(0.956863, 1.0, 0.505882);
+constexpr glm::vec3 LimeA200(0.933333, 1.0, 0.254902);
+constexpr glm::vec3 LimeA400(0.776471, 1.0, 0.0);
+constexpr glm::vec3 LimeA700(0.682353, 0.917647, 0.0);
+
+constexpr glm::vec3 Yellow50(1.0, 0.992157, 0.905882);
+constexpr glm::vec3 Yellow100(1.0, 0.976471, 0.768627);
+constexpr glm::vec3 Yellow200(1.0, 0.960784, 0.615686);
+constexpr glm::vec3 Yellow300(1.0, 0.945098, 0.462745);
+constexpr glm::vec3 Yellow400(1.0, 0.933333, 0.345098);
+constexpr glm::vec3 Yellow500(1.0, 0.921569, 0.231373);
+constexpr glm::vec3 Yellow600(0.992157, 0.847059, 0.207843);
+constexpr glm::vec3 Yellow700(0.984314, 0.752941, 0.176471);
+constexpr glm::vec3 Yellow800(0.976471, 0.658824, 0.145098);
+constexpr glm::vec3 Yellow900(0.960784, 0.498039, 0.090196);
+constexpr glm::vec3 YellowA100(1.0, 1.0, 0.552941);
+constexpr glm::vec3 YellowA200(1.0, 1.0, 0.0);
+constexpr glm::vec3 YellowA400(1.0, 0.917647, 0.0);
+constexpr glm::vec3 YellowA700(1.0, 0.839216, 0.0);
+
+constexpr glm::vec3 Amber50(1.0, 0.972549, 0.882353);
+constexpr glm::vec3 Amber100(1.0, 0.92549, 0.701961);
+constexpr glm::vec3 Amber200(1.0, 0.878431, 0.509804);
+constexpr glm::vec3 Amber300(1.0, 0.835294, 0.309804);
+constexpr glm::vec3 Amber400(1.0, 0.792157, 0.156863);
+constexpr glm::vec3 Amber500(1.0, 0.756863, 0.027451);
+constexpr glm::vec3 Amber600(1.0, 0.701961, 0.0);
+constexpr glm::vec3 Amber700(1.0, 0.627451, 0.0);
+constexpr glm::vec3 Amber800(1.0, 0.560784, 0.0);
+constexpr glm::vec3 Amber900(1.0, 0.435294, 0.0);
+constexpr glm::vec3 AmberA100(1.0, 0.898039, 0.498039);
+constexpr glm::vec3 AmberA200(1.0, 0.843137, 0.25098);
+constexpr glm::vec3 AmberA400(1.0, 0.768627, 0.0);
+constexpr glm::vec3 AmberA700(1.0, 0.670588, 0.0);
+
+constexpr glm::vec3 Orange50(1.0, 0.952941, 0.878431);
+constexpr glm::vec3 Orange100(1.0, 0.878431, 0.698039);
+constexpr glm::vec3 Orange200(1.0, 0.8, 0.501961);
+constexpr glm::vec3 Orange300(1.0, 0.717647, 0.301961);
+constexpr glm::vec3 Orange400(1.0, 0.654902, 0.14902);
+constexpr glm::vec3 Orange500(1.0, 0.596078, 0.0);
+constexpr glm::vec3 Orange600(0.984314, 0.54902, 0.0);
+constexpr glm::vec3 Orange700(0.960784, 0.486275, 0.0);
+constexpr glm::vec3 Orange800(0.937255, 0.423529, 0.0);
+constexpr glm::vec3 Orange900(0.901961, 0.317647, 0.0);
+constexpr glm::vec3 OrangeA100(1.0, 0.819608, 0.501961);
+constexpr glm::vec3 OrangeA200(1.0, 0.670588, 0.25098);
+constexpr glm::vec3 OrangeA400(1.0, 0.568627, 0.0);
+constexpr glm::vec3 OrangeA700(1.0, 0.427451, 0.0);
+
+constexpr glm::vec3 DeepOrange50(0.984314, 0.913725, 0.905882);
+constexpr glm::vec3 DeepOrange100(1.0, 0.8, 0.737255);
+constexpr glm::vec3 DeepOrange200(1.0, 0.670588, 0.568627);
+constexpr glm::vec3 DeepOrange300(1.0, 0.541176, 0.396078);
+constexpr glm::vec3 DeepOrange400(1.0, 0.439216, 0.262745);
+constexpr glm::vec3 DeepOrange500(1.0, 0.341176, 0.133333);
+constexpr glm::vec3 DeepOrange600(0.956863, 0.317647, 0.117647);
+constexpr glm::vec3 DeepOrange700(0.901961, 0.290196, 0.098039);
+constexpr glm::vec3 DeepOrange800(0.847059, 0.262745, 0.082353);
+constexpr glm::vec3 DeepOrange900(0.74902, 0.211765, 0.047059);
+constexpr glm::vec3 DeepOrangeA100(1.0, 0.619608, 0.501961);
+constexpr glm::vec3 DeepOrangeA200(1.0, 0.431373, 0.25098);
+constexpr glm::vec3 DeepOrangeA400(1.0, 0.239216, 0.0);
+constexpr glm::vec3 DeepOrangeA700(0.866667, 0.172549, 0.0);
+
+constexpr glm::vec3 Brown50(0.937255, 0.921569, 0.913725);
+constexpr glm::vec3 Brown100(0.843137, 0.8, 0.784314);
+constexpr glm::vec3 Brown200(0.737255, 0.666667, 0.643137);
+constexpr glm::vec3 Brown300(0.631373, 0.533333, 0.498039);
+constexpr glm::vec3 Brown400(0.552941, 0.431373, 0.388235);
+constexpr glm::vec3 Brown500(0.47451, 0.333333, 0.282353);
+constexpr glm::vec3 Brown600(0.427451, 0.298039, 0.254902);
+constexpr glm::vec3 Brown700(0.364706, 0.25098, 0.215686);
+constexpr glm::vec3 Brown800(0.305882, 0.203922, 0.180392);
+constexpr glm::vec3 Brown900(0.243137, 0.152941, 0.137255);
+
+constexpr glm::vec3 Gray50(0.980392, 0.980392, 0.980392);
+constexpr glm::vec3 Gray100(0.960784, 0.960784, 0.960784);
+constexpr glm::vec3 Gray200(0.933333, 0.933333, 0.933333);
+constexpr glm::vec3 Gray300(0.878431, 0.878431, 0.878431);
+constexpr glm::vec3 Gray400(0.741176, 0.741176, 0.741176);
+constexpr glm::vec3 Gray500(0.619608, 0.619608, 0.619608);
+constexpr glm::vec3 Gray600(0.458824, 0.458824, 0.458824);
+constexpr glm::vec3 Gray700(0.380392, 0.380392, 0.380392);
+constexpr glm::vec3 Gray800(0.258824, 0.258824, 0.258824);
+constexpr glm::vec3 Gray900(0.129412, 0.129412, 0.129412);
+
+constexpr glm::vec3 BlueGray50(0.92549, 0.937255, 0.945098);
+constexpr glm::vec3 BlueGray100(0.811765, 0.847059, 0.862745);
+constexpr glm::vec3 BlueGray200(0.690196, 0.745098, 0.772549);
+constexpr glm::vec3 BlueGray300(0.564706, 0.643137, 0.682353);
+constexpr glm::vec3 BlueGray400(0.470588, 0.564706, 0.611765);
+constexpr glm::vec3 BlueGray500(0.376471, 0.490196, 0.545098);
+constexpr glm::vec3 BlueGray600(0.329412, 0.431373, 0.478431);
+constexpr glm::vec3 BlueGray700(0.270588, 0.352941, 0.392157);
+constexpr glm::vec3 BlueGray800(0.215686, 0.278431, 0.309804);
+constexpr glm::vec3 BlueGray900(0.14902, 0.196078, 0.219608);
+
+constexpr glm::vec3 Black(0);
+constexpr glm::vec3 White(1);
+
+}  // namespace color
+}  // namespace uinta
+
+#endif  // SRC_ENGINE_INCLUDE_UINTA_COLOR_H_
