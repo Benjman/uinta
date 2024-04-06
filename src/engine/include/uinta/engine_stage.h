bed824c29a6cea90c8a4367fcd11307cbe2c9407
Ben <benjamin.s.record@gmail.com>
2024-04-06 08:18:11 -0600

    `EngineStage`

---
+++ src/engine/include/uinta/engine_stage.h
@@ -0,0 +1,20 @@
+#ifndef SRC_ENGINE_INCLUDE_UINTA_ENGINE_STAGE_H_
+#define SRC_ENGINE_INCLUDE_UINTA_ENGINE_STAGE_H_
+
+#include "uinta/types.h"
+
+namespace uinta {
+
+enum class EngineStage : u8 {
+  NewFrame,
+  PreTick,
+  Tick,
+  PostTick,
+  PreRender,
+  Render,
+  PostRender,
+};
+
+}  // namespace uinta
+
+#endif  // SRC_ENGINE_INCLUDE_UINTA_ENGINE_STAGE_H_
