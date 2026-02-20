dae8b05b7d8fea3c79a39f6536bb39b0935a53a1
Ben Record <benjamin.s.record@gmail.com>
2026-02-20 16:20:43 -0700

    demo: `AxisViewerScene`

--- src/engine/src/debug/debug_scene.cc
+++ src/engine/src/debug/debug_scene.cc
@@ -1,11 +1,13 @@
 #include "uinta/debug/debug_scene.h"
 
 #include "uinta/debug/debug_scene_ui.h"
+#include "uinta/scenes/axis_viewer.h"
 
 namespace uinta {
 
 DebugScene::DebugScene(Scene* parent) noexcept : Scene(parent) {
   addScene<DebugSceneUi>();
+  addScene<AxisViewerScene>();
 }
 
 }  // namespace uinta
