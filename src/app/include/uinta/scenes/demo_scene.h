f3e16135fee3e4d00c93010b1176fb7dbdcc16d6
Ben Record <benjamin.s.record@gmail.com>
2026-03-22 12:34:43 -0600

    demo: Ugly trees

--- src/app/include/uinta/scenes/demo_scene.h
+++ src/app/include/uinta/scenes/demo_scene.h
@@ -12,6 +12,7 @@
 #include "uinta/scenes/fbx_viewer_scene.h"
 #include "uinta/scenes/manifold_scene.h"
 #include "uinta/scenes/terrain_scene.h"
+#include "uinta/scenes/trees_scene.h"
 #include "uinta/shaders/basic_shader.h"
 
 namespace uinta {
@@ -32,6 +33,7 @@ class DemoScene : public Scene {
     manifoldScene_ = addScene<ManifoldScene>();
     fbxViewerScene_ = addScene<FbxViewerScene>();
     terrainScene_ = addScene<TerrainScene>();
+    treeScene_ = addScene<TreeScene>();
   }
 
   void render(time_t delta) noexcept override {
@@ -51,6 +53,7 @@ class DemoScene : public Scene {
   ManifoldScene* manifoldScene_ = nullptr;
   FbxViewerScene* fbxViewerScene_ = nullptr;
   TerrainScene* terrainScene_ = nullptr;
+  TreeScene* treeScene_ = nullptr;
 
   time_t runtime_ = 0.0;
 };
---
+++ src/app/include/uinta/scenes/trees_scene.h
@@ -0,0 +1,141 @@
+#ifndef SRC_APP_INCLUDE_UINTA_SCENES_TREES_SCENE_H_
+#define SRC_APP_INCLUDE_UINTA_SCENES_TREES_SCENE_H_
+
+#include <absl/random/random.h>
+#include <imgui.h>
+
+#include "uinta/debug/debug_scene_ui.h"
+#include "uinta/engine/engine.h"
+#include "uinta/flags.h"
+#include "uinta/gl.h"
+#include "uinta/math/defs.h"
+#include "uinta/platform.h"
+#include "uinta/scene/scene.h"
+#include "uinta/shaders/basic_shader.h"
+#include "uinta/vbo.h"
+
+namespace uinta {
+
+class TreeScene : public Scene {
+ public:
+  explicit TreeScene(Scene* parent) noexcept
+      : Scene(parent, SceneLayer::Simulation),
+        vbo_(GL_ARRAY_BUFFER, 0, engine()->gl()),
+        bsm_(parent->findComponent<BasicShaderManager>().value_or(nullptr)),
+        input_(parent->engine()->input()) {
+    if (bsm_ == nullptr) {
+      engine()->setStatusError(
+          absl::NotFoundError("BasicShaderManager* not found."));
+    }
+    generateTrees();
+  }
+
+  void preRender(time_t /*unused*/) noexcept override {
+    if (input_->isMousePressed(MOUSE_BUTTON_LEFT)) {
+      leftMouseDownPosition = {input_->cursorx(), input_->cursory()};
+    } else if (input_->isMouseReleased(MOUSE_BUTTON_LEFT)) {
+      auto cursorDelta =
+          glm::length(glm::vec2(input_->cursorx(), input_->cursory()) -
+                      leftMouseDownPosition) < epsilon_f;
+      if (cursorDelta) {
+        addTree(glm::translate(glm::mat4(1), input_->cursorWorldPoint()));
+      }
+    }
+  }
+
+  void onDebugUi() noexcept override {
+    ImGui::Text("Render count: %zu", indexCount_);
+    ImGui::PushItemWidth(50);
+    ImGui::DragFloat("Trees per meter", &treesPerMeter_, 0.01, 0.0, 1, "%3.2f");
+    ImGui::PopItemWidth();
+    ImGui::SameLine();
+    if (ImGui::Button("Generate")) {
+      vtxSegment_ = BufferSegment();
+      idxSegment_ = BufferSegment();
+      idxOffset_ = 0;
+      indexCount_ = 0;
+      generateTrees();
+    }
+    auto flags = flags_.as<i32>();
+    if (ImGui::CheckboxFlags("Sway", &flags, Flags::SwayMask)) {
+      flags_.isSway((flags & Flags::SwayMask) != 0);
+    }
+  }
+
+  void generateTrees() noexcept {
+    absl::BitGen bitgen;
+    absl::uniform_real_distribution<f32> random(-distrobutionSize_.x * 0.5f,
+                                                distrobutionSize_.y * 0.5f);
+    for (auto i = treeCount(); std::round(i) != 0; i--) {
+      addTree(glm::translate(glm::mat4(1),
+                             glm::vec3(random(bitgen), 0, random(bitgen))));
+    }
+  }
+
+  void render(time_t /*unused*/) noexcept override {
+    DepthTestGuard dtg;
+    CullFaceGuard cfg;
+    ShaderGuard sg(bsm_->shader());
+    VaoGuard vg(&vao_);
+
+    bsm_->model(glm::scale(glm::mat4(1), glm::vec3(1)));
+    bsm_->sway(flags_.isSway());
+    bsm_->drawElements(GL_TRIANGLES, static_cast<GLsizei>(indexCount_),
+                       GL_UNSIGNED_INT, nullptr);
+  }
+
+  void addTree(glm::mat4 transform) noexcept {
+    auto mesh = Mesh::Environment::Tree(&idxOffset_, transform);
+    auto* data = mesh.vertices().data();
+    auto size = mesh.vertices().size() * Vertex::ElementCount * sizeof(f32);
+    VboGuard vbg(&vbo_);
+    vtxSegment_ = vbo_.bufferData(data, static_cast<GLsizei>(size),
+                                  GL_STATIC_DRAW, vtxSegment_);
+
+    VaoGuard vag(&vao_);
+    idxSegment_ = vao_.ebo(mesh.elements(), idxSegment_);
+    indexCount_ += mesh.elements().size();
+    bsm_->linkAttributes(&vao_);
+  }
+
+ private:
+  struct Flags final {
+    using value_type = u8;
+
+    static constexpr value_type SwayMask = 1 << 0;
+
+    FlagsOperations(SwayMask);
+
+    [[nodiscard]] bool isSway() const noexcept {
+      return (flags_ & SwayMask) != 0;
+    }
+    void isSway(bool v) noexcept {
+      flags_ &= ~SwayMask;
+      if (v) {
+        flags_ |= SwayMask;
+      }
+    }
+
+    value_type flags_;
+  } flags_;
+  Vao vao_;
+  Vbo vbo_;
+  BufferSegment vtxSegment_;
+  BufferSegment idxSegment_;
+  size_t indexCount_ = 0;
+  idx_t idxOffset_ = 0;
+  f32 treesPerMeter_ = 0.05;
+  glm::vec2 distrobutionSize_ = {32, 32};
+  glm::vec2 leftMouseDownPosition;
+  BasicShaderManager* bsm_;
+  const Input* input_;
+
+  i32 treeCount() const noexcept {
+    return static_cast<i32>(
+        std::round(treesPerMeter_ * distrobutionSize_.x * distrobutionSize_.y));
+  }
+};
+
+}  // namespace uinta
+
+#endif  // SRC_APP_INCLUDE_UINTA_SCENES_TREES_SCENE_H_
