355a1668077b1b6d4df2d9cf75c21d411e9f3702
Ben Record <benjamin.s.record@gmail.com>
2026-03-22 12:34:43 -0600

    demo: City prototype (WIP)

---
+++ src/app/include/uinta/scenes/city_game/city_game_scene.h
@@ -0,0 +1,39 @@
+#ifndef SRC_APP_INCLUDE_UINTA_SCENES_CITY_GAME_CITY_GAME_SCENE_SCENE_H_
+#define SRC_APP_INCLUDE_UINTA_SCENES_CITY_GAME_CITY_GAME_SCENE_SCENE_H_
+
+#include "./intersection_data.h"
+#include "./intersection_renderer.h"
+#include "./pawn_data.h"
+#include "./pawn_renderer.h"
+#include "./pawn_types.h"
+#include "./sim_time.h"
+#include "uinta/scene/scene.h"
+
+namespace uinta {
+
+class CityGameScene : public Scene {
+ public:
+  explicit CityGameScene(Scene* parent) noexcept;
+
+  void postTick(time_t dt) noexcept override;
+  void render(time_t dt) noexcept override;
+  void onDebugUi() noexcept override;
+
+ private:
+  // Simulation data
+  IntersectionBuildings buildings_;
+  Pawns pawns_;
+  SimTime simTime_;
+  PawnConfig pawnConfig_;
+
+  // Rendering
+  IntersectionRenderer intersectionRenderer_;
+  PawnRenderer pawnRenderer_;
+
+  // Simulation control
+  bool simulationPaused_ = false;
+};
+
+}  // namespace uinta
+
+#endif  // SRC_APP_INCLUDE_UINTA_SCENES_CITY_GAME_CITY_GAME_SCENE_SCENE_H_
