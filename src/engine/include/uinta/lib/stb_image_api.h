a8ea5300d32ec3abf0616e29cf62af621c52ca50
Ben Record <benjamin.s.record@gmail.com>
2026-04-08 22:31:20 -0600

    feat: `Texture`

---
+++ src/engine/include/uinta/lib/stb_image_api.h
@@ -0,0 +1,40 @@
+#ifndef SRC_ENGINE_INCLUDE_UINTA_LIB_STB_IMAGE_API_H_
+#define SRC_ENGINE_INCLUDE_UINTA_LIB_STB_IMAGE_API_H_
+
+#include "uinta/types.h"
+
+namespace uinta {
+
+class StbImageApi {
+ public:
+  virtual void setFlipVerticallyOnLoad(bool) const noexcept = 0;
+  virtual u8* load(char const*, i32*, i32*, i32*, i32) const noexcept = 0;
+  virtual void imageFree(void*) const noexcept = 0;
+};
+
+class StbImageApiImpl : public StbImageApi {
+ public:
+  static const StbImageApi* Instance() noexcept {
+    static StbImageApiImpl instance;
+    return &instance;
+  }
+
+  ~StbImageApiImpl() noexcept = default;
+
+  StbImageApiImpl(const StbImageApiImpl&) noexcept = delete;
+  StbImageApiImpl& operator=(const StbImageApiImpl&) noexcept = delete;
+  StbImageApiImpl(StbImageApiImpl&&) noexcept = delete;
+  StbImageApiImpl& operator=(StbImageApiImpl&&) noexcept = delete;
+
+  void setFlipVerticallyOnLoad(bool /*unused*/) const noexcept override;
+  u8* load(char const* /*unused*/, i32* /*unused*/, i32* /*unused*/,
+           i32* /*unused*/, i32 /*unused*/) const noexcept override;
+  void imageFree(void* /*unused*/) const noexcept override;
+
+ private:
+  StbImageApiImpl() noexcept = default;
+};
+
+}  // namespace uinta
+
+#endif  // SRC_ENGINE_INCLUDE_UINTA_LIB_STB_IMAGE_API_H_
