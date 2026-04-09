783c4d5603be90d238ae07d82b121883e1c5ef92
Ben Record <benjamin.s.record@gmail.com>
2026-04-08 22:31:19 -0600

    lib: abseil-cpp

--- src/engine/src/engine.cc
+++ src/engine/src/engine.cc
@@ -1,11 +1,14 @@
 #include "uinta/engine/engine.h"
 
+#include <absl/log/log.h>
+
 #include <cassert>
 #include <string>
 
 namespace uinta {
 
 std::string Engine::Message() const noexcept {
+  LOG(INFO) << "Message request recieved.";
   return "Hello from Uinta Engine\n";
 }
 
